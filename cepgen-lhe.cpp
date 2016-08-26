616d52a809db3488fb78d0ab37c9b327cb783ea1
Laurent <laurent.forthomme@cern.ch>
2016-08-26 17:50:55 +0200

    Cleanup of the HepMC export code ; changed the event building logic (simpler wrt the not-so-generic class of processes used here)

--- cepgen-lhe.cpp
+++ cepgen-lhe.cpp
@@ -43,67 +43,10 @@ int main(int argc, char* argv[]) {
     if (i%10000==0)
       cout << "Generating event #" << i+1 << endl;
     const Event ev = *mg.GenerateOneEvent();
-    ev.Dump();
     HepMC::GenEvent* hev = EventWriter::HepMC::Event(ev);
     hev->set_cross_section(xs);                                                                                                                                                                                                                                      
     hev->set_event_number(i); 
 
-    /*std::vector<HepMC::GenParticle*> hepmc_particles;
-    for (int iprt=0; iprt<2; iprt++) {
-      Particle* p = part[iprt]; Particle::Momentum p_m = p->GetMomentum();
-      HepMC::FourVector pmom(p_m.Px(), p_m.Py(), p_m.Pz(), p_m.E());
-      HepMC::GenParticle* primary = new HepMC::GenParticle(pmom, p->GetIntPDGId(), p->status); //FIXME
-      primary->suggest_barcode(iprt+1);
-      hepmc_particles.push_back(primary);
-    }
-    HepMC::GenVertex* v1 = new HepMC::GenVertex(HepMC::FourVector(0., 0., 0., 0.));
-    HepMC::GenVertex* v2 = new HepMC::GenVertex(HepMC::FourVector(0., 0., 0., 0.));
-    v1->add_particle_in(hepmc_particles[0]);
-    v2->add_particle_in(hepmc_particles[1]);
-    hev->add_vertex(v1);
-    hev->add_vertex(v2);
-    hev->set_beam_particles(hepmc_particles[0], hepmc_particles[1]);
-    //
-    HepMC::GenVertex* DecVtx = 0;
-    for (int iprt=2; iprt<part.size(); iprt++) {
-      Particle* p = part[iprt]; Particle::Momentum p_m = p->GetMomentum();
-      int parent = *p->GetMothersIds().begin();
-      HepMC::FourVector pmom(p_m.Px(), p_m.Py(), p_m.Pz(), p_m.E());
-      HepMC::GenParticle* daughter = new HepMC::GenParticle(pmom, p->GetIntPDGId(), p->status); //FIXME
-      daughter->suggest_barcode(iprt+1);
-      if (parent==0) {
-        HepMC::GenVertex* v21 = new HepMC::GenVertex(HepMC::FourVector(0., 0., 0., 0.));
-        v21->add_particle_in(daughter);
-        hev->add_vertex(v21);
-      }
-      else if (parent==1) { v1->add_particle_out(daughter); }
-      else if (parent==2) { v2->add_particle_out(daughter); }
-      else {
-        HepMC::GenParticle* parentPart = hev->barcode_to_particle(parent);
-        parentPart->set_status(2); // reset status, to mark that it's decayed
-        DecVtx = new HepMC::GenVertex(HepMC::FourVector(0., 0., 0., 0.));
-        DecVtx->add_particle_in(parentPart);
-        DecVtx->add_particle_out(daughter);
-        hev->add_vertex(DecVtx);
-      }
-      int iprt1;
-      for (iprt1=iprt+1; iprt1<part.size(); iprt1++) { // the pointer is shifted by -1, c++ style
-        Particle* p = part[iprt]; Particle::Momentum p_m = p->GetMomentum();
-        if (*p->GetMothersIds().begin()!=parent) break; // another parent particle, break the loop
-        HepMC::FourVector pmomN(p_m.Px(), p_m.Py(), p_m.Pz(), p_m.E());
-        HepMC::GenParticle* daughterN = new HepMC::GenParticle(pmomN, p->GetIntPDGId(), p->status); //FIXME
-        daughterN->suggest_barcode(iprt1+1);
-        if (parent==1) { v1->add_particle_out(daughterN); }
-        else if (parent==2) { v2->add_particle_out(daughterN); }
-        else { DecVtx->add_particle_out(daughterN); }
-      }
-      iprt = iprt1-1; // reset counter such that it doesn't go over the same child more than once
-      // don't forget to offset back into c++ counting, as it's already +1 forward
-    }
-    hev->set_signal_process_vertex(*(v1->vertices_begin()));*/
-
-
-    //ev.Dump();
     output << (hev);
   }
 
--- export/EventWriter.cpp
+++ export/EventWriter.cpp
@@ -9,70 +9,77 @@ namespace EventWriter
     {
       if (last_event) { delete last_event; }
       last_event = new ::HepMC::GenEvent;
-      //GenEvent hev;
       const ::HepMC::FourVector origin(0., 0., 0., 0.);
-      ::HepMC::GenVertex *v1, *v2, *DecVtx=0;
+      ::HepMC::GenVertex *v1, *v2, *vcm;
 
       ConstParticlesRef part = ev.GetConstParticlesRef();
 
       std::vector<::HepMC::GenParticle*> hepmc_particles;
+      int cm_id = 0, idx = 1;
       for (unsigned int j=0; j<part.size(); j++) {
         const Particle* p = part[j]; Particle::Momentum p_m = p->GetMomentum();
         ::HepMC::FourVector pmom(p_m.Px(), p_m.Py(), p_m.Pz(), p_m.E());
         ::HepMC::GenParticle* gp = new ::HepMC::GenParticle(pmom, p->GetIntPDGId(), p->status);
-        gp->suggest_barcode(j+1);
         hepmc_particles.push_back(gp);
 
-        p->Dump();
-
-        if (j==0) { // first incoming proton
-          v1 = new ::HepMC::GenVertex(origin);
-          v1->add_particle_in(gp);
-          last_event->add_vertex(v1);
-          continue;
+        switch (p->role) {
+          case Particle::IncomingBeam1: {
+            gp->suggest_barcode(idx++);
+            v1 = new ::HepMC::GenVertex(origin);
+            v1->add_particle_in(gp);
+            last_event->add_vertex(v1);
+          } break;
+          case Particle::IncomingBeam2: {
+            gp->suggest_barcode(idx++);
+            v2 = new ::HepMC::GenVertex(origin);
+            v2->add_particle_in(gp);
+            last_event->add_vertex(v2);
+          } break;
+          case Particle::OutgoingBeam1: {
+            gp->suggest_barcode(idx++);
+            v1->add_particle_out(gp);
+          } break;
+          case Particle::OutgoingBeam2: {
+            gp->suggest_barcode(idx++);
+            v2->add_particle_out(gp);
+          } break;
+          case Particle::Parton1: {
+            gp->suggest_barcode(idx++);
+            v1->add_particle_out(gp);
+            vcm = new ::HepMC::GenVertex(origin);
+            vcm->add_particle_in(gp);
+            last_event->add_vertex(vcm);
+          } break;
+          case Particle::Parton2: {
+            gp->suggest_barcode(idx++);
+            v2->add_particle_out(gp);
+            vcm->add_particle_in(gp);
+          } break;
+          case Particle::Parton3: {
+            gp->suggest_barcode(idx++);
+            v2->add_particle_out(gp);
+            vcm->add_particle_in(gp);
+          } break;
+          case Particle::CentralSystem: {
+            cm_id = j;
+          } break;
+          case Particle::CentralParticle1:
+          case Particle::CentralParticle2:
+          default: {
+            gp->suggest_barcode(idx++);
+            const ParticlesIds moth = p->GetMothersIds();
+            if (moth.size()==0) { continue; }
+            if (*moth.begin()==cm_id) {
+              vcm->add_particle_out(gp);
+            }
+            else {
+              //FIXME secondary products... to be implemented!
+            }
+          } break;
         }
-        if (j==1) { // second incoming proton
-          v2 = new ::HepMC::GenVertex(origin);
-          v2->add_particle_in(gp);
-          last_event->add_vertex(v2);
-          continue;
-        }
-
-        ParticlesIds moth = p->GetMothersIds();
-        if (moth.size()==0) {
-          std::cout << "particle with no mother:" << std::endl;
-          p->Dump();
-          continue;
-        }
-	
-        const int parent = *moth.begin();
-        if (parent==0) { v1->add_particle_out(gp); }
-        else if (parent==1) { v2->add_particle_out(gp); }
-	else {
-	  std::cout << "retrieving particle with barcode " << parent+1 << std::endl;
-	  ::HepMC::GenParticle* parentPart = last_event->barcode_to_particle(parent+1);
-	  if (!parentPart) continue; //FIXME
-	  parentPart->set_status(2); // reset status, to mark that it's decayed
-	  DecVtx = new ::HepMC::GenVertex(origin);
-	  DecVtx->add_particle_in(parentPart);
-	  DecVtx->add_particle_out(gp);
-	  last_event->add_vertex(DecVtx);
-	}
-        for (unsigned int k=j+1; k<part.size(); k++) { // the pointer is shifted by -1, c++ style
-          const Particle* p2 = part[k]; Particle::Momentum p_m2 = p2->GetMomentum();
-	  std::cout << "---> retrieving next particle with id=" << p2->id << " and parent=" << *p2->GetMothersIds().begin() << std::endl; 
-          if (*p2->GetMothersIds().begin()!=parent) break; // another parent particle, break the loop
-          ::HepMC::FourVector pmomN(p_m2.Px(), p_m2.Py(), p_m2.Pz(), p_m2.E());
-          ::HepMC::GenParticle* daughterN = new ::HepMC::GenParticle(pmomN, p2->GetIntPDGId(), p2->status); //FIXME
-          daughterN->suggest_barcode(k+1);
-          if (parent==1) { v1->add_particle_out(daughterN); continue; }
-          if (parent==2) { v2->add_particle_out(daughterN); continue; }
-          DecVtx->add_particle_out(daughterN);
-	}
       }
       last_event->set_beam_particles(hepmc_particles[0], hepmc_particles[1]);
       last_event->set_signal_process_vertex(*(v1->vertices_begin()));
-last_event->print();
       return last_event;
     }
   }
