dc68cfa53567846f581fba49145a6324809490bd
Laurent <laurent.forthomme@cern.ch>
2016-09-26 16:43:20 +0200

    Added the timer object stripped back from utils

---
+++ core/Timer.h
@@ -0,0 +1,33 @@
+#ifndef Timer_h
+#define Timer_h
+
+#include <time.h>
+
+/**
+ * A generic timer to extract the processing time between two steps in this software's flow
+ * \author Laurent Forthomme <laurent.forthomme@cern.ch>
+ */
+class Timer
+{
+ public:
+  inline Timer() { clock_gettime(CLOCK_REALTIME, &beg_); }
+  /**
+   * Get the time elapsed since the last @a reset call (or class construction)
+   * @return Elapsed time (since the last reset), in seconds
+   */
+  inline double elapsed() {
+    clock_gettime(CLOCK_REALTIME, &end_);
+    return end_.tv_sec -beg_.tv_sec+(end_.tv_nsec - beg_.tv_nsec)/1.e9;
+  }
+  /// Reset the clock counter
+  inline void reset() {
+    clock_gettime(CLOCK_REALTIME, &beg_);
+  }
+ private:
+  /// Timestamp marking the beginning of the counter
+  timespec beg_;
+  /// Timestamp marking the end of the counter
+  timespec end_;
+};
+
+#endif
