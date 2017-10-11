afcb2031741bf58cafed1bd83bb0deaa737bb64c
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-10-11 13:46:49 +0200

    Yet another simplification

--- CepGen/IO/FortranInterface.cpp
+++ CepGen/IO/FortranInterface.cpp
@@ -1,12 +1,14 @@
-#include "CepGen/IO/FortranInterface.h"
-
 #include "CepGen/StructureFunctions/StructureFunctionsBuilder.h"
 
-void
-cepgen_structure_functions_( int& sfmode, double& q2, double& xbj, double& f2, double& fl )
+extern "C"
 {
-  const CepGen::StructureFunctions sf = CepGen::StructureFunctionsBuilder::get( ( CepGen::StructureFunctions::Type )sfmode, q2, xbj );
-  f2 = sf.F2;
-  fl = sf.FL;
+  void
+  cepgen_structure_functions_( int& sfmode, double& q2, double& xbj, double& f2, double& fl )
+  {
+    const CepGen::StructureFunctions::Type mode = ( CepGen::StructureFunctions::Type )sfmode;
+    const CepGen::StructureFunctions sf = CepGen::StructureFunctionsBuilder::get( mode, q2, xbj );
+    f2 = sf.F2;
+    fl = sf.FL;
+  }
 }
 
--- test/test_f77_interface.f
+++ test/test_f77_interface.f
@@ -6,11 +6,11 @@
       double precision xbj,min_xbj,max_xbj
       double precision q2
 
-      nsf=104 ! GD11p
+      nsf=204 ! GD11p
       q2=0.225
       min_xbj=1.0d-3
       max_xbj=1.0
-      niter=100
+      niter=101
 
       do i=1,niter
          xbj=min_xbj+(max_xbj-min_xbj)*(i-1)/(niter-1)
