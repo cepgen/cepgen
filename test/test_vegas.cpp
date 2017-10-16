2fe5391479b301d4522587d67c16294b81c044ab
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-10-16 10:46:35 +0200

    Extra test for Vegas

--- test/test_vegas.cpp
+++ test/test_vegas.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int
 main( int argc, char* argv[] )
 {
-  //CepGen::Logger::get().level = CepGen::Logger::Nothing;
+  CepGen::Logger::get().level = CepGen::Logger::Nothing;
 
   CepGen::Generator mg;
   mg.parameters->vegas.ncvg = 500000;
@@ -24,12 +24,19 @@ main( int argc, char* argv[] )
     if ( fabs( exact - result ) > 2.0 * error ) throw CepGen::Exception( __PRETTY_FUNCTION__, Form( "pull = %.5e", fabs( exact-result )/error ), CepGen::FatalError );
     cout << "Test 1 passed!" << endl;
   }
-  { // test 2 // functional bug to be solved!!
+  { // test 2
     const double exact = 2./3.;
     mg.parameters->setProcess( new CepGen::Process::TestProcess<2>( "x^2+y^2", { { "x", "y" } } ) );
     mg.computeXsection( result, error );
     if ( fabs( exact - result ) > 2.0 * error ) throw CepGen::Exception( __PRETTY_FUNCTION__, Form( "pull = %.5e", fabs( exact-result )/error ), CepGen::FatalError );
     cout << "Test 2 passed!" << endl;
+  }
+  { // test 3
+    const double exact = 13./12.;
+    mg.parameters->setProcess( new CepGen::Process::TestProcess<3>( "x+y^2+z^3", { { "x", "y", "z" } } ) );
+    mg.computeXsection( result, error );
+    if ( fabs( exact - result ) > 2.0 * error ) throw CepGen::Exception( __PRETTY_FUNCTION__, Form( "pull = %.5e", fabs( exact-result )/error ), CepGen::FatalError );
+    cout << "Test 3 passed!" << endl;
   }
 
   return 0;
