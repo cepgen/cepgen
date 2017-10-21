711c118583c4502ba2916d91fe0ff5c6d11f4b6e
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-10-21 14:03:14 +0200

    Improved F2_luxlike (delegated the sub-structure functions type retrieval from the main routine to F2_CONT/F2_RES subroutines)

--- External/F2_Luxlike_fit/F2_luxlike_minimal.f
+++ External/F2_Luxlike_fit/F2_luxlike_minimal.f
@@ -21,30 +21,22 @@ c       -----------------------------
               F2 = F2p
               FL = FLp
            else
-              call F2_cont(xbj,q2,F2c,FLc)
+              call F2_cont(xbj,q2,icont_model,F2c,FLc)
               F2 = F2c
               FL = FLc
            endif
         else ! Q2 < Q2cut
            if(w2.le.w2_lo) then
-              if(ires_model.eq.1) then     ! Christy-Bosted
-                 call CepGen_Structure_Functions(102,Q2,xbj,F2r,FLr)
-              elseif(ires_model.eq.2) then ! Fiore-Brasse
-                 call CepGen_Structure_Functions(101,Q2,xbj,F2r,FLr)
-              endif
+              call F2_res(xbj,q2,ires_model,F2r,FLr)
               F2 = F2r
               FL = FLr
            elseif(w2.gt.w2_lo.and.w2.le.w2_hi) then
-              if(ires_model.eq.1) then     ! Christy-Bosted
-                 call CepGen_Structure_Functions(102,Q2,xbj,F2r,FLr)
-              elseif(ires_model.eq.2) then ! Fiore-Brasse
-                 call CepGen_Structure_Functions(101,Q2,xbj,F2r,FLr)
-              endif
-              call F2_cont(xbj,q2,F2c,FLc)
+              call F2_res(xbj,q2,ires_model,F2r,FLr)
+              call F2_cont(xbj,q2,icont_model,F2c,FLc)
               F2 = (1.d0-rho)*F2r + rho*F2c
               FL = (1.d0-rho)*FLr + rho*FLc
            elseif(w2.gt.w2_hi) then
-              call F2_cont(xbj,q2,F2c,FLc)
+              call F2_cont(xbj,q2,icont_model,F2c,FLc)
               F2 = F2c
               FL = FLc
            endif
@@ -53,20 +45,30 @@ c       -----------------------------
         return
         end
 c       ---------------------------------------------------------
-        subroutine F2_CONT(xbj,q2,F2c,FLc)
+        subroutine F2_RES(xbj,q2,irm,F2r,FLr)
         implicit real*8 (a-h,o-z)
-        common/luxlike_params/amp,am_pi,alpha_em,
-     &     q2_cut,w2_lo,w2_hi,
-     &     ires_model,icont_model
-        if(icont_model.eq.1) then     ! GD11p
+        if(irm.eq.1) then     ! Christy-Bosted
+           call CepGen_Structure_Functions(102,Q2,xbj,F2,FL)
+        elseif(irm.eq.2) then ! Fiore-Brasse
+           call CepGen_Structure_Functions(101,Q2,xbj,F2,FL)
+        endif
+        F2r = F2
+        FLr = FL
+
+        return
+        end
+c       ---------------------------------------------------------
+        subroutine F2_CONT(xbj,q2,icm,F2c,FLc)
+        implicit real*8 (a-h,o-z)
+        if(icm.eq.1) then     ! GD11p
            call CepGen_Structure_Functions(204,Q2,xbj,F2,FL)
-        elseif(icont_model.eq.2) then ! ALLM91
+        elseif(icm.eq.2) then ! ALLM91
            call CepGen_Structure_Functions(201,Q2,xbj,F2,FL)
-        elseif(icont_model.eq.3) then ! ALLM97
+        elseif(icm.eq.3) then ! ALLM97
            call CepGen_Structure_Functions(202,Q2,xbj,F2,FL)
         endif
         F2c = F2
-        FLc = F2c
+        FLc = FL
 
         return
         end
