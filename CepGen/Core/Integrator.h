438ec1eea2d367c03d459357c6ba01f4d76781bd
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-02-25 07:54:02 +0100

    Improved documentation for integrator

--- CepGen/Core/Integrator.h
+++ CepGen/Core/Integrator.h
@@ -17,10 +17,11 @@ namespace cepgen
   class Event;
   class GridParameters;
   namespace utils { class Timer; }
+  /// Flavour of integration algorithm
   enum class IntegratorType {
-    plain = 0,
-    Vegas = 1, ///< VEGAS algorithm \cite Lepage:1977sw developed by G.P. Lepage
-    MISER = 2
+    plain = 0, ///< Simple trial-and-error algorithm
+    Vegas = 1, ///< Vegas algorithm (G.P. Lepage, 1977 \cite Lepage:1977sw)
+    MISER = 2  ///< MISER stratified sampling algorithm
   };
   /// Monte-Carlo integrator instance
   class Integrator
@@ -50,13 +51,15 @@ namespace cepgen
       int integrate( double& result_, double& abserr_ );
       /// Dimensional size of the phase space
       unsigned short dimensions() const;
+      /// Generate a single event
       void generateOne( std::function<void( const Event&, unsigned long )> callback = nullptr );
+      /// Launch the event generation for a given number of events
       void generate( unsigned long num_events = 0, std::function<void( const Event&, unsigned long )> callback = nullptr );
 
     private:
       /**
-       * Store the event characterized by its _ndim-dimensional point in the phase
-       * space to the output file
+       * Store the event characterized by its _ndim-dimensional point in
+       * the phase space to the output file
        * \brief Store the event in the output file
        * \param[in] x The d-dimensional point in the phase space defining the unique event to store
        * \return A boolean stating whether or not the event could be saved
@@ -66,29 +69,40 @@ namespace cepgen
       /// \param x Point in the phase space considered
       /// \param has_correction Correction cycle started?
       bool correctionCycle( std::vector<double>& x, bool& has_correction );
+      /// Prepare Vegas for an integration/event generation cycle
       int warmupVegas( std::vector<double>& x_low, std::vector<double>& x_up, unsigned int ncall );
       /**
-       * Set all the generation mode variables and align them to the integration grid set while computing the cross-section
+       * Set all the generation mode variables and align them to the
+       *  integration grid set while computing the cross-section
        * \brief Prepare the class for events generation
        */
       void computeGenerationParameters();
+      /// Generate a uniformly distributed (between 0 and 1) random number
       double uniform() const;
+      /// Compute the function value at the given phase space point
       double eval( const std::vector<double>& x );
       /// Selected bin at which the function will be evaluated
       int ps_bin_;
       static constexpr int INVALID_BIN = -1;
-      /// List of parameters to specify the integration range and the physics determining the phase space
+      /// List of parameters to specify the integration range and the
+      /// physics determining the phase space
       std::shared_ptr<Parameters> input_params_;
-      /// GSL structure storing the function to be integrated by this integrator instance (along with its parameters)
+      /// GSL structure storing the function to be integrated by this
+      /// integrator instance (along with its parameters)
       std::unique_ptr<gsl_monte_function> function_;
+      /// Instance of random number generator service
       std::unique_ptr<gsl_rng,void(*)( gsl_rng* )> rng_;
+      /// Set of parameters for the integration/event generation grid
       std::unique_ptr<GridParameters> grid_;
+      /// A trivial deleter for the Vegas integrator
       struct gsl_monte_vegas_deleter
       {
         void operator()( gsl_monte_vegas_state* state ) {
           gsl_monte_vegas_free( state );
         }
       };
+      /// A Vegas integrator state for integration (optional) and/or
+      /// "treated" event generation
       std::unique_ptr<gsl_monte_vegas_state,gsl_monte_vegas_deleter> veg_state_;
   };
   std::ostream& operator<<( std::ostream&, const IntegratorType& );
