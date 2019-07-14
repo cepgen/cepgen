1ddbd065b3ddb1366694f1da4d58bdcc9aca741e
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-07-14 13:45:18 +0200

    HepMC version specified at module launch

--- Cards/lpair_cfg.py
+++ Cards/lpair_cfg.py
@@ -30,6 +30,7 @@ process = cepgen.Module('lpair',
 #--- example of an output module parameterisation
 #output = cepgen.Module('text', variables = ['m(4)'])
 #output = cepgen.Module('lhef', output = 'test.lhe')
+#output = cepgen.Module('hepmc', output = 'test.hepmc')
 
 #--- let the user specify the run conditions
 from Config.generator_cff import generator
