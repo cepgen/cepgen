b268cf2dd5bbad0e15080085b70f725d387a144f
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-07-22 15:48:45 +0200

    Imported the new HepMC event interface from the tauola branch

---
+++ CepGen/IO/HepMCEventInterface.h
@@ -0,0 +1,51 @@
+#ifndef CepGen_IO_HepMCEventInterface_h
+#define CepGen_IO_HepMCEventInterface_h
+
+namespace cepgen {
+  class Parameters;
+  class Event;
+  class Particle;
+}
+
+#ifdef HEPMC3
+#  include "HepMC3/Version.h"
+#  include "HepMC3/GenEvent.h"
+#  define HepMC HepMC3
+#else
+#  include "HepMC/Version.h"
+#  include "HepMC/GenEvent.h"
+#endif
+
+namespace HepMC
+{
+  /// Interfacing between CepGen and HepMC event definitions
+  /// \author Laurent Forthomme <laurent.forthomme@cern.ch>
+  /// \date Jul 2019
+  class CepGenEvent : public GenEvent
+  {
+    public:
+      explicit CepGenEvent();
+      /// Initialise this conversion object with CepGen parameters
+      void initialise( const cepgen::Parameters& );
+      /// Feed a new CepGen event to this conversion object
+      /// \param[in] ev CepGen event to be fed
+      void feedEvent( const cepgen::Event& ev );
+      /// Set the cross section for a given process
+      /// \param[in] id Process identifier
+      /// \param[in] xsec Process cross section, in pb
+      /// \param[in] xsec_err Uncertainty on process cross section, in pb
+      void setCrossSection( int id, double xsec, double xsec_err );
+      /// Specify new process attributes
+      /// \param[in] id Process identifier
+      /// \param[in] xsec Process cross section, in pb
+      /// \param[in] q2_scale Hard event scale \f$Q^2\f$, in GeV\f$^2\f$
+      /// \param[in] alpha_qed \f$\alpha_{\rm em}\f$ for this process
+      /// \param[in] alpha_qcd \f$\alpha_{\rm s}\f$ for this process
+      void setProcess( int id, double xsec, double q2_scale, double alpha_qed, double alpha_qcd );
+
+    private:
+      const cepgen::Parameters* params_; // borrowed
+  };
+}
+#endif
+
--- CepGen/IO/HepMCHandler.cpp
+++ CepGen/IO/HepMCHandler.cpp
@@ -1,4 +1,5 @@
 #include "CepGen/IO/ExportHandler.h"
+#include "CepGen/IO/HepMCEventInterface.h"
 
 #include "CepGen/Parameters.h"
 #include "CepGen/Core/Exception.h"
@@ -8,7 +9,6 @@
 #include "CepGen/Physics/Constants.h"
 
 #ifdef HEPMC3
-#  include "HepMC3/Version.h"
 #  define HEPMC_VERSION HEPMC3_VERSION
 #  include "HepMC3/WriterAscii.h"
 #  include "HepMC3/WriterAsciiHepMC2.h"
@@ -17,34 +17,17 @@
 #    include "HepMC3/WriterRoot.h"
 #    include "HepMC3/WriterRootTree.h"
 #  endif
-#  include "HepMC3/FourVector.h"
-#  include "HepMC3/GenEvent.h"
-#  include "HepMC3/GenRunInfo.h"
-#  include "HepMC3/GenVertex.h"
-#  include "HepMC3/GenParticle.h"
-using namespace HepMC3;
+#elif !defined( HEPMC_VERSION_CODE ) // HepMC v2
+#  include "HepMC/IO_GenEvent.h"
 #else
-#  include "HepMC/Version.h"
-#  if !defined( HEPMC_VERSION_CODE ) // HepMC v2
-#    include "HepMC/IO_GenEvent.h"
-#    include "HepMC/SimpleVector.h"
-#    include "HepMC/GenEvent.h"
-#    include "HepMC/GenVertex.h"
-#    include "HepMC/GenParticle.h"
-#  else
-#    include "HepMC/WriterAscii.h"
-#    include "HepMC/WriterHEPEVT.h"
-#    include "HepMC/FourVector.h"
-#    include "HepMC/GenEvent.h"
-#    include "HepMC/GenRunInfo.h"
-#    include "HepMC/GenVertex.h"
-#    include "HepMC/GenParticle.h"
-#    define HEPMC3
-#  endif
-using namespace HepMC;
+#  include "HepMC/WriterAscii.h"
+#  include "HepMC/WriterHEPEVT.h"
+#  define HEPMC3
 #endif
 
 #include <memory>
+
+using namespace HepMC;
 
 namespace cepgen
 {
@@ -60,7 +43,7 @@ namespace cepgen
       public:
         /// Class constructor
         explicit HepMCHandler( const ParametersList& );
-        void initialise( const Parameters& /*params*/ ) override;
+        void initialise( const Parameters& /*params*/ ) override {}
         /// Writer operator
         void operator<<( const Event& ) override;
         void setCrossSection( double, double ) override;
@@ -80,7 +63,7 @@ namespace cepgen
         std::shared_ptr<GenRunInfo> runinfo_;
 #endif
         /// Associated HepMC event
-        std::shared_ptr<GenEvent> event_;
+        std::shared_ptr<CepGenEvent> event_;
     };
 
     template<typename T>
@@ -91,7 +74,7 @@ namespace cepgen
 #ifdef HEPMC3
       runinfo_( new GenRunInfo ),
 #endif
-      event_( new GenEvent( Units::GEV, Units::MM ) )
+      event_( new CepGenEvent )
     {
 #ifdef HEPMC3
       output_->set_run_info( runinfo_ );
@@ -114,18 +97,6 @@ namespace cepgen
     }
 
     template<typename T> void
-    HepMCHandler<T>::initialise( const Parameters& )
-    {
-#ifdef HEPMC3
-      event_->add_attribute( "AlphaQCD", make_shared<DoubleAttribute>( constants::ALPHA_QCD ) );
-      event_->add_attribute( "AlphaEM", make_shared<DoubleAttribute>( constants::ALPHA_EM ) );
-#else
-      event_->set_alphaQCD( constants::ALPHA_QCD );
-      event_->set_alphaQED( constants::ALPHA_EM );
-#endif
-    }
-
-    template<typename T> void
     HepMCHandler<T>::setCrossSection( double xsect, double xsect_err )
     {
       xs_->set_cross_section( xsect, xsect_err );
@@ -134,8 +105,7 @@ namespace cepgen
     template<typename T> void
     HepMCHandler<T>::fillEvent( const Event& evt )
     {
-      event_->clear();
-
+      event_->feedEvent( evt );
       // general information
 #ifdef HEPMC3
       event_->set_cross_section( xs_ );
@@ -143,68 +113,7 @@ namespace cepgen
 #else
       event_->set_cross_section( *xs_ );
 #endif
-
-      event_->set_event_number( event_num_ );
-      event_->weights().push_back( 1. ); // unweighted events
-
-      // filling the particles content
-      const FourVector origin( 0., 0., 0., 0. );
-      Particles part_vec = evt.particles();
-
-      int cm_id = 0, idx = 1;
-
-#ifdef HEPMC3
-      GenVertexPtr v1 = make_shared<GenVertex>( origin ),
-                   v2 = make_shared<GenVertex>( origin ),
-                   vcm = make_shared<GenVertex>( origin );
-#else
-      GenVertex* v1 = new GenVertex( origin ),
-                *v2 = new GenVertex( origin ),
-                *vcm = new GenVertex( origin );
-#endif
-      for ( unsigned int i = 0; i < part_vec.size(); ++i ) {
-        const Particle part_orig = part_vec.at( i );
-        FourVector pmom( part_orig.momentum().px(),
-                                part_orig.momentum().py(),
-                                part_orig.momentum().pz(),
-                                part_orig.energy() );
-#ifdef HEPMC3
-        auto part = make_shared<GenParticle>( pmom, part_orig.integerPdgId(), (int)part_orig.status() );
-#else
-        auto part = new GenParticle( pmom, part_orig.integerPdgId(), (int)part_orig.status() );
-        part->suggest_barcode( idx++ );
-#endif
-        const ParticlesIds moth = part_orig.mothers();
-
-        switch ( part_orig.role() ) {
-          case Particle::IncomingBeam1: { v1->add_particle_in( part ); } break;
-          case Particle::IncomingBeam2: { v2->add_particle_in( part ); } break;
-          case Particle::OutgoingBeam1: { v1->add_particle_out( part ); } break;
-          case Particle::OutgoingBeam2: { v2->add_particle_out( part ); } break;
-          case Particle::Parton1:       { v1->add_particle_out( part ); vcm->add_particle_in( part ); } break;
-          case Particle::Parton2:       { v2->add_particle_out( part ); vcm->add_particle_in( part ); } break;
-          case Particle::Intermediate:  { cm_id = i; continue; } break;
-          case Particle::CentralSystem:
-          default: {
-            if ( moth.empty() ) continue;
-            if ( *moth.begin() == cm_id ) vcm->add_particle_out( part );
-            else
-              throw CG_FATAL( "HepMCHandler:fillEvent" )
-                << "Other particle requested! Not yet implemented!";
-          } break;
-        }
-        idx++;
-      }
-      event_->add_vertex( v1 );
-      event_->add_vertex( v2 );
-      event_->add_vertex( vcm );
-
-#ifndef HEPMC3
-      event_->set_beam_particles( *v1->particles_in_const_begin(), *v2->particles_in_const_begin() );
-      event_->set_signal_process_vertex( *v1->vertices_begin() );
-      event_->set_beam_particles( *v1->particles_in_const_begin(), *v2->particles_in_const_end() );
-#endif
-      event_num_++;
+      event_->set_event_number( event_num_++ );
     }
 #ifdef HEPMC3
 #  if HEPMC_VERSION_CODE >= 3001000
