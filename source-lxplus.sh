83bc69bb76c221d617b88ce79b91ef9290166b13
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-08-11 18:35:54 +0200

    Addded root include path for modules/external retrieval in delphes

--- source-lxplus.sh
+++ source-lxplus.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+#--- proper gcc/gfortran version
 source /cvmfs/sft.cern.ch/lcg/releases/gcc/6.2.0/x86_64-centos7/setup.sh
-source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-81463/x86_64-centos7-gcc62-opt/bin/thisroot.sh #--- for Delphes
+#--- for Delphes
+source /cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-81463/x86_64-centos7-gcc62-opt/bin/thisroot.sh
+export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:/cvmfs/sft.cern.ch/lcg/releases/delphes/3.4.2pre12-1a2e9/x86_64-centos7-gcc62-opt/include
+#--- extra utilities
 export PYTHONHOME=/cvmfs/sft.cern.ch/lcg/releases/Python/2.7.15-075d4/x86_64-centos7-gcc62-opt
 export PYTHIA8_DIR=/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/240p1-ecd34/x86_64-centos7-gcc62-opt
 export PYTHIA8DATA=${PYTHIA8_DIR}/share/Pythia8/xmldoc
+
 echo "Environment prepared for LXPLUS"
