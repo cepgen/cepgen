8293c99b35eeb94b133de18ea552328d57ba682f
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-10-22 17:46:22 +0200

    Less hardcoded constants

--- CepGen/IO/MCDFileParser.cpp
+++ CepGen/IO/MCDFileParser.cpp
@@ -8,6 +8,14 @@
 
 namespace pdg
 {
+  const std::unordered_map<std::string,short> MCDFileParser::m_charge_str_ = {
+    { "-", -3 }, { "--", -6 },
+    { "+", +3 }, { "++", +6 },
+    { "0", 0 },
+    { "-1/3", -1 }, { "-2/3", -2 },
+    { "+1/3", +1 }, { "+2/3", +2 }
+  };
+
   void
   MCDFileParser::parse( const char* path )
   {
@@ -22,46 +30,35 @@ namespace pdg
       double width, width_err_low, width_err_high;
       std::string part_name, part_charge_int;
       { // pdg ids
-        std::istringstream ss( line.substr( 1, 32 ) );
+        std::istringstream ss( line.substr( PDG_BEG, PDG_END ) );
         std::string buf;
         // split for each PDG id
         while ( ss >> buf )
           pdg_ids.emplace_back( std::stoi( buf ) );
       }
       { // mass + error(s)
-        std::istringstream oss( line.substr( 33, 70 ) );
+        std::istringstream oss( line.substr( MASS_BEG, MASS_END ) );
         oss
           >> mass >> mass_err_low >> mass_err_high;
       }
       { // width + error(s)
-        std::istringstream oss( line.substr( 70, 107 ) );
+        std::istringstream oss( line.substr( WIDTH_BEG, WIDTH_END ) );
         oss
           >> width >> width_err_low >> width_err_high;
       }
       { // name + charge
-        std::istringstream oss( line.substr( 107 ) );
+        std::istringstream oss( line.substr( AUX_BEG ) );
         oss
           >> part_name >> part_charge_int;
         std::istringstream oss_ch( part_charge_int );
         std::string charge_int;
         // split by ','
         while ( std::getline( oss_ch, charge_int, ',' ) ) {
-          short charge = 0;
-          if ( charge_int == "-" )
-            charge = -3;
-          else if ( charge_int == "+" )
-            charge = +3;
-          else if ( charge_int == "--" )
-            charge = -6;
-          else if ( charge_int == "++" )
-            charge = +6;
-          else if ( charge_int == "0" )
-            charge = 0;
-          else if ( charge_int == "-1/3" )
-            charge = -1;
-          else if ( charge_int == "+2/3" )
-            charge = +2;
-          charges.emplace_back( charge );
+          if ( m_charge_str_.count( charge_int ) == 0 )
+            throw CG_FATAL( "MCDFileParser" )
+              << "Failed to retrieve an integer charge "
+              << "for string \"" << charge_int << "\"!";
+          charges.emplace_back( m_charge_str_.at( charge_int ) );
         }
       }
       if ( pdg_ids.size() != charges.size() )
--- CepGen/IO/MCDFileParser.h
+++ CepGen/IO/MCDFileParser.h
@@ -9,5 +9,10 @@ namespace pdg
       static void parse( const char* path );
 
     private:
+      static constexpr size_t PDG_BEG = 1, PDG_END = 33;
+      static constexpr size_t MASS_BEG = 33, MASS_END = 70;
+      static constexpr size_t WIDTH_BEG = 70, WIDTH_END = 107;
+      static constexpr size_t AUX_BEG = 107;
+      static const std::unordered_map<std::string,short> m_charge_str_;
   };
 }
