a901b72c268ce42402355d894751e38dddfd2043
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-10-29 12:23:59 +0100

    Now also handling 1D profiles

--- CepGen/IO/ROOTHistsHandler.cpp
+++ CepGen/IO/ROOTHistsHandler.cpp
@@ -14,6 +14,7 @@
 #include "TH1.h"
 #include "TH2.h"
 #include "TH3.h"
+#include "TProfile.h"
 
 namespace cepgen
 {
@@ -37,6 +38,7 @@ namespace cepgen
       private:
         std::unique_ptr<TFile> file_;
         std::vector<std::pair<std::string,TH1*> > hists1d_;
+        std::vector<std::pair<std::vector<std::string>,TProfile*> > profiles1d_;
         std::vector<std::pair<std::vector<std::string>,TH2*> > hists2d_;
         std::vector<std::pair<std::vector<std::string>,TH3*> > hists3d_;
 
@@ -64,28 +66,36 @@ namespace cepgen
         nbins_x = hvars.get<int>( "nbins", nbins_x );
         double min_x = hvars.get<double>( "lowX", 0. ), max_x = hvars.get<double>( "highX", 1. );
         min_x = hvars.get<double>( "low", min_x ), max_x = hvars.get<double>( "high", max_x );
+        const bool profile = hvars.get<bool>( "profile", false );
         if ( vars.size() == 1 ) { // 1D histogram
           const auto title = Form( "%s;%s;d#sigma/d(%s) (pb/bin)", key.c_str(), key.c_str(), key.c_str() );
-          hists1d_.emplace_back( std::make_pair( vars[0], new TH1D( key.c_str(), title.c_str(),
-            nbins_x, min_x, max_x ) ) );
+          hists1d_.emplace_back( std::make_pair( key,
+            new TH1D( key.c_str(), title.c_str(), nbins_x, min_x, max_x ) ) );
           CG_INFO( "ROOTHistsHandler" )
-            << "Booking a histogram with " << utils::s( "bin", nbins_x )
-            << " between " << min_x << " and " << max_x << " for \"" << vars[0] << "\".";
+            << "Booking a 1D histogram with " << utils::s( "bin", nbins_x )
+            << " between " << min_x << " and " << max_x
+            << " for \"" << key << "\".";
           continue;
         }
         const int nbins_y = hvars.get<int>( "nbinsY", 10 );
         const double min_y = hvars.get<double>( "lowY", 0. ), max_y = hvars.get<double>( "highY", 1. );
-        if ( vars.size() == 2 ) { // 2D histogram
+        if ( vars.size() == 2 ) { // 2D histogram / 1D profile
           const auto title = Form( "(%s / %s) correlation;%s;%s;d^{2}#sigma/d(%s)/d(%s) (pb/bin)",
             vars[0].c_str(), vars[1].c_str(),
             vars[0].c_str(), vars[1].c_str(),
             vars[0].c_str(), vars[1].c_str() );
-          hists2d_.emplace_back( std::make_pair( vars, new TH2D( key.c_str(), title.c_str(),
-            nbins_x, min_x, max_x, nbins_y, min_y, max_y ) ) );
+          if ( profile )
+            profiles1d_.emplace_back( std::make_pair( vars,
+              new TProfile( key.c_str(), title.c_str(), nbins_x, min_x, max_x ) ) );
+          else
+            hists2d_.emplace_back( std::make_pair( vars,
+              new TH2D( key.c_str(), title.c_str(), nbins_x, min_x, max_x, nbins_y, min_y, max_y ) ) );
           CG_INFO( "ROOTHistsHandler" )
-            << "Booking a 2D correlation plot with " << utils::s( "bin", nbins_x+nbins_y )
-            << " between (" << min_x << ", " << min_y << ") and (" << max_x << ", " << max_y << ") "
-            << "for \"" << merge( vars, " / " ) << "\".";
+            << "Booking a "
+            << ( profile ? "1D profile" : "2D correlation plot" )
+            << " with " << utils::s( "bin", nbins_x+nbins_y )
+            << " between (" << min_x << ", " << min_y << ") and (" << max_x << ", " << max_y << ")"
+            << " for \"" << merge( vars, " / " ) << "\".";
           continue;
         }
         const int nbins_z = hvars.get<int>( "nbinsZ", 10 );
@@ -95,8 +105,8 @@ namespace cepgen
             vars[0].c_str(), vars[1].c_str(), vars[2].c_str(),
             vars[0].c_str(), vars[1].c_str(), vars[2].c_str(),
             vars[0].c_str(), vars[1].c_str(), vars[2].c_str() );
-          hists3d_.emplace_back( std::make_pair( vars, new TH3D( key.c_str(), title.c_str(),
-            nbins_x, min_x, max_x, nbins_y, min_y, max_y, nbins_z, min_z, max_z ) ) );
+          hists3d_.emplace_back( std::make_pair( vars,
+            new TH3D( key.c_str(), title.c_str(), nbins_x, min_x, max_x, nbins_y, min_y, max_y, nbins_z, min_z, max_z ) ) );
           CG_INFO( "ROOTHistsHandler" )
             << "Booking a 3D correlation plot with " << utils::s( "bin", nbins_x+nbins_y+nbins_z )
             << " between (" << min_x << ", " << min_y << ", " << min_z << ")"
@@ -112,6 +122,8 @@ namespace cepgen
       //--- finalisation of the output file
       for ( const auto& hist : hists1d_ )
         hist.second->Write( hist.first.c_str() );
+      for ( const auto& hist : profiles1d_ )
+        hist.second->Write( merge( hist.first, "vs" ).c_str() );
       for ( const auto& hist : hists2d_ )
         hist.second->Write( merge( hist.first, "_" ).c_str() );
       for ( const auto& hist : hists3d_ )
@@ -127,6 +139,10 @@ namespace cepgen
       for ( const auto& h_var : hists1d_ )
         h_var.second->Fill(
           browser_.get( ev, h_var.first ), xsec_ );
+      for ( const auto& h_var : profiles1d_ )
+        h_var.second->Fill(
+          browser_.get( ev, h_var.first[0] ),
+          browser_.get( ev, h_var.first[1] ), xsec_ );
       for ( const auto& h_var : hists2d_ )
         h_var.second->Fill(
           browser_.get( ev, h_var.first[0] ),
