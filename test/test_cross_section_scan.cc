c90030fb167cf857735088b9a726f367d0e93b9f
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-02-22 14:22:56 +0100

    Fixed case where ROOT is used as functionals parser

--- test/test_cross_section_scan.cc
+++ test/test_cross_section_scan.cc
@@ -17,6 +17,7 @@ int main( int argc, char* argv[] )
   string proc_name, output_file;
   int proc_mode, npoints;
   double min_value, max_value, sqrts;
+  double min_eta, max_eta;
 
   cepgen::ArgumentsParser( argc, argv )
     .addArgument( "proc-name", "name of the process to scan", &proc_name, 'p' )
@@ -26,6 +27,8 @@ int main( int argc, char* argv[] )
     .addArgument( "max-value", "maximum value of scan", &max_value, 'H' )
     .addOptionalArgument( "output", "output file", "xsect.dat", &output_file, 'o' )
     .addOptionalArgument( "sqrts", "c.o.m. energy", 13.e3, &sqrts, 'w' )
+    .addOptionalArgument( "min-eta", "minimum central system pseudorapidity", -2.5, &min_eta )
+    .addOptionalArgument( "max-eta", "maximum central system pseudorapidity", +2.5, &max_eta )
     .parse();
 
   cepgen::Generator mg;
@@ -34,7 +37,7 @@ int main( int argc, char* argv[] )
 
   cepgen::Parameters& par = mg.parameters();
   par.kinematics.setSqrtS( sqrts );
-  par.kinematics.cuts.central.eta_single = { -2.5, 2.5 };
+  par.kinematics.cuts.central.eta_single = { min_eta, max_eta };
   par.kinematics.cuts.remnants.mass_single.max() = 1000.0;
   par.setProcess( cepgen::proc::ProcessesFactory::get().build( proc_name ) );
   par.kinematics.mode = static_cast<cepgen::KinematicsMode>( proc_mode );
