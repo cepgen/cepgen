988c68a63fc787a8ebfb48db9fd2247e59c1f4be
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-04-28 09:41:57 +0200

    Python3-ready cards handler

--- CepGen/Cards/PythonTypes.cpp
+++ CepGen/Cards/PythonTypes.cpp
@@ -67,7 +67,7 @@ namespace cepgen
       return PyInt_AsUnsignedLongMask( obj );
 #else
       if ( !PyLong_Check( obj ) )
-        throwPythonError( utils::format( "Object \"%s\" has invalid type: unsigned long != %s", key, obj->ob_type->tp_name ) );
+        throwPythonError( utils::format( "Object has invalid type: unsigned long != %s", obj->ob_type->tp_name ) );
       return PyLong_AsUnsignedLong( obj );
 #endif
     }
