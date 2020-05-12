9a16729992702a6cf558522427532d38df2ef42e
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-05-12 22:54:36 +0200

    Updated doc path for upload

--- upload-doc.sh
+++ upload-doc.sh
@@ -1,1 +1,1 @@
-rsync -arvz -e 'ssh -p 222' --delete docs/docs/ lforthomme@login.hepforge.org:~/cepgen/public_html/
+rsync -arvz -e 'ssh -p 222' --delete doc/docs/ lforthomme@login.hepforge.org:~/cepgen/public_html/
