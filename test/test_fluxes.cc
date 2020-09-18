f553db9c2671af388044f8767e64469a8fd317cd
Laurent Forthomme <laurent.forthomme@cern.ch>
2020-09-18 13:29:13 +0200

    Fixed fluxes test

--- test/test_fluxes.cc
+++ test/test_fluxes.cc
@@ -2,6 +2,7 @@
 
 #include "CepGen/Core/Exception.h"
 #include "CepGen/Physics/KTFlux.h"
+#include "CepGen/Physics/FormFactors.h"
 #include "CepGen/Physics/PDG.h"
 
 #include "CepGen/Modules/StructureFunctionsFactory.h"
@@ -12,11 +13,12 @@
 
 int main( int argc, char* argv[] )
 {
-  int strfun_type, num_points;
+  int formfac_type, strfun_type, num_points;
   double kt2, mx;
   std::string output_file;
 
   cepgen::ArgumentsParser( argc, argv )
+    .addOptionalArgument( "ff,f", "form factors modelling", &formfac_type, 1 )
     .addOptionalArgument( "sf,s", "structure functions modelling", &strfun_type, 301 )
     .addOptionalArgument( "kt2,k", "parton transverse virtuality (GeV^2)", &kt2, 100. )
     .addOptionalArgument( "mx,m", "diffractive state mass (GeV)", &mx, 1.5 )
@@ -28,19 +30,22 @@ int main( int argc, char* argv[] )
   const double mi = cepgen::PDG::get().mass( cepgen::PDG::proton );
   const double mi2 = mi*mi, mx2 = mx*mx;
 
+  auto ff = cepgen::formfac::FormFactorsFactory::get().build( formfac_type );
   auto sf = cepgen::strfun::StructureFunctionsFactory::get().build( strfun_type );
+  ff->setStructureFunctions( sf.get() );
   std::ofstream out( output_file );
   out
-    << "# structure functions: " << sf->description() << "\n"
+    << "# form factors: " << ff.get() << "\n"
+    << "# structure functions: " << sf.get() << "\n"
     << "# kt2 = " << kt2 << " GeV^2\n"
     << "# mX = " << mx << " GeV\n";
   for ( int i = 0; i < num_points; ++i ) {
     const double x = i*1./num_points;
     out << x
-      << "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Photon_Elastic, x, kt2, *sf, mi2, mx2 )
-      << "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Photon_Inelastic_Budnev, x, kt2, *sf, mi2, mx2 )
-      //<< "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Gluon_KMR, x, kt2, *sf, mi2, mx2 )
-      //<< "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Gluon_KMR_alt, x, kt2, *sf, mi2, mx2 )
+      << "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Photon_Elastic, x, kt2, *ff, mi2, mx2 )
+      << "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Photon_Inelastic_Budnev, x, kt2, *ff, mi2, mx2 )
+      //<< "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Gluon_KMR, x, kt2, *ff, mi2, mx2 )
+      //<< "\t" << cepgen::ktFlux( cepgen::KTFlux::P_Gluon_KMR_alt, x, kt2, *ff, mi2, mx2 )
       << "\n";
   }
   CG_INFO( "main" )
