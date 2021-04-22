a11870eb74660b3a2a7b10f21bda26fc2eca3591
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-04-22 19:26:13 +0200

    Imported back the python configuration files

--- Cards/Config/Core.py
+++ Cards/Config/Core.py
@@ -5,6 +5,11 @@ A collection of tools for Python steering cards definition
 '''
 
 #--- core components includes
+from .containers_cfi import Module, Parameters, Sequence
+from .Logger_cfi import Logging
 
 #--- physics-level includes
+from .StructureFunctions_cfi import StructureFunctions
+from .SigmaRatio_cfi import SigmaRatio
+from .ProcessMode_cfi import ProcessMode
 
