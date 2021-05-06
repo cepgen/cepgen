44a5a607d69e25c8308fab3b060a7929bbd3c6c5
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-05-06 11:19:37 +0200

    MG5: Clang-format

--- CepGenAddOns/MadGraphWrapper/MadGraphInterface.h
+++ CepGenAddOns/MadGraphWrapper/MadGraphInterface.h
@@ -10,80 +10,66 @@
 // forward-declaration of base MadGraph standalone_cpp process
 class CPPProcess;
 
-namespace cepgen
-{
-  class MadGraphInterface
-  {
-    public:
-      MadGraphInterface( const ParametersList& );
+namespace cepgen {
+  class MadGraphInterface {
+  public:
+    MadGraphInterface(const ParametersList&);
 
-      std::string run() const;
+    std::string run() const;
 
-    private:
-      static std::string runCommand( const std::string& );
-      static std::string generateLibrary( const std::string&, const std::string&, const std::string& );
-      static std::string generateProcess( const std::string& );
+  private:
+    static std::string runCommand(const std::string&);
+    static std::string generateLibrary(const std::string&, const std::string&, const std::string&);
+    static std::string generateProcess(const std::string&);
 
-      void prepareCard() const;
-      std::string prepareMadGraphProcess() const;
-      void linkCards() const;
+    void prepareCard() const;
+    std::string prepareMadGraphProcess() const;
+    void linkCards() const;
 
-      const std::string proc_;
-      const std::string model_;
-      const std::string card_path_;
-      const std::string standalone_cpp_path_;
-      const std::string tmp_dir_;
-      const std::string log_filename_;
+    const std::string proc_;
+    const std::string model_;
+    const std::string card_path_;
+    const std::string standalone_cpp_path_;
+    const std::string tmp_dir_;
+    const std::string log_filename_;
 
-      const std::unordered_map<std::string,pdgid_t> mg5_parts_ = {
-        { "d", (pdgid_t)1 }, { "d~", (pdgid_t)1 },
-        { "u", (pdgid_t)2 }, { "u~", (pdgid_t)2 },
-        { "s", (pdgid_t)3 }, { "s~", (pdgid_t)3 },
-        { "c", (pdgid_t)4 }, { "c~", (pdgid_t)4 },
-        { "b", (pdgid_t)5 }, { "b~", (pdgid_t)5 },
-        { "t", (pdgid_t)6 }, { "t~", (pdgid_t)6 },
-        { "e+", (pdgid_t)11 }, { "e-", (pdgid_t)11 },
-        { "ve", (pdgid_t)12 }, { "ve~", (pdgid_t)12 },
-        { "mu+", (pdgid_t)13 }, { "mu-", (pdgid_t)13 },
-        { "vm", (pdgid_t)14 }, { "vm~", (pdgid_t)14 },
-        { "tau+", (pdgid_t)15 }, { "tau-", (pdgid_t)15 },
-        { "vt", (pdgid_t)16 }, { "vt~", (pdgid_t)16 },
-        { "g", (pdgid_t)21 },
-        { "a", (pdgid_t)22 },
-        { "z", (pdgid_t)23 },
-        { "w+", (pdgid_t)24 }, { "w-", (pdgid_t)24 },
-        { "h", (pdgid_t)25 },
-      };
+    const std::unordered_map<std::string, pdgid_t> mg5_parts_ = {
+        {"d", (pdgid_t)1},     {"d~", (pdgid_t)1},    {"u", (pdgid_t)2},    {"u~", (pdgid_t)2},   {"s", (pdgid_t)3},
+        {"s~", (pdgid_t)3},    {"c", (pdgid_t)4},     {"c~", (pdgid_t)4},   {"b", (pdgid_t)5},    {"b~", (pdgid_t)5},
+        {"t", (pdgid_t)6},     {"t~", (pdgid_t)6},    {"e+", (pdgid_t)11},  {"e-", (pdgid_t)11},  {"ve", (pdgid_t)12},
+        {"ve~", (pdgid_t)12},  {"mu+", (pdgid_t)13},  {"mu-", (pdgid_t)13}, {"vm", (pdgid_t)14},  {"vm~", (pdgid_t)14},
+        {"tau+", (pdgid_t)15}, {"tau-", (pdgid_t)15}, {"vt", (pdgid_t)16},  {"vt~", (pdgid_t)16}, {"g", (pdgid_t)21},
+        {"a", (pdgid_t)22},    {"z", (pdgid_t)23},    {"w+", (pdgid_t)24},  {"w-", (pdgid_t)24},  {"h", (pdgid_t)25},
+    };
   };
 
   /// Wrapper around a generic MadGraph CPPProcess definition
-  class MadGraphProcess
-  {
-    public:
-      MadGraphProcess();
-      ~MadGraphProcess();
+  class MadGraphProcess {
+  public:
+    MadGraphProcess();
+    ~MadGraphProcess();
 
-      const std::string& name() const { return name_; }
-      const std::string& description() const { return descr_; }
+    const std::string& name() const { return name_; }
+    const std::string& description() const { return descr_; }
 
-      void initialise( const std::string& );
-      double eval();
+    void initialise(const std::string&);
+    double eval();
 
-      MadGraphProcess& setMomentum( size_t, const Momentum& );
-      const std::vector<Momentum>& momenta();
-      const std::vector<double>& masses() const;
+    MadGraphProcess& setMomentum(size_t, const Momentum&);
+    const std::vector<Momentum>& momenta();
+    const std::vector<double>& masses() const;
 
-    private:
-      std::unique_ptr<CPPProcess> proc_;
-      std::vector<double*> mom_;
+  private:
+    std::unique_ptr<CPPProcess> proc_;
+    std::vector<double*> mom_;
 
-      const std::string name_;
-      const std::string descr_;
-      const std::array<int,2> incoming_pdgids_;
-      const std::vector<int> central_pdgids_;
+    const std::string name_;
+    const std::string descr_;
+    const std::array<int, 2> incoming_pdgids_;
+    const std::vector<int> central_pdgids_;
 
-      std::vector<Momentum> momenta_;
+    std::vector<Momentum> momenta_;
   };
-}
+}  // namespace cepgen
 
 #endif
--- CepGenAddOns/MadGraphWrapper/MadGraphProcessBuilder.cpp
+++ CepGenAddOns/MadGraphWrapper/MadGraphProcessBuilder.cpp
@@ -9,51 +9,46 @@
 
 using namespace cepgen;
 
-class MadGraphProcessBuilder : public proc::Process2to4
-{
-  public:
-    MadGraphProcessBuilder( const ParametersList& params );
-    proc::ProcessPtr clone() const override {
-      return proc::ProcessPtr( new MadGraphProcessBuilder( *this ) );
-    }
-    static std::string description() { return "MadGraph_aMC process builder"; }
+class MadGraphProcessBuilder : public proc::Process2to4 {
+public:
+  MadGraphProcessBuilder(const ParametersList& params);
+  proc::ProcessPtr clone() const override { return proc::ProcessPtr(new MadGraphProcessBuilder(*this)); }
+  static std::string description() { return "MadGraph_aMC process builder"; }
 
-    void prepareProcessKinematics() override;
-    double computeCentralMatrixElement() const override;
+  void prepareProcessKinematics() override;
+  double computeCentralMatrixElement() const override;
 
-    /*void preparePhaseSpace() override;
+  /*void preparePhaseSpace() override;
     double computeKTFactorisedMatrixElement() override;
     void fillCentralParticlesKinematics() override;*/
 
-  private:
-    std::shared_ptr<MadGraphProcess> mg5_proc_;
+private:
+  std::shared_ptr<MadGraphProcess> mg5_proc_;
 };
 
 extern std::string madgraph_process_name();
 
-MadGraphProcessBuilder::MadGraphProcessBuilder( const ParametersList& params ) :
-  //KTProcess( params, std::array<pdgid_t,2>{}, std::vector<pdgid_t>{} )
-  Process2to4( params, std::array<pdgid_t,2>{}, 0 )
-{
-  if ( params.has<std::string>( "lib" ) )
-    loadLibrary( params.get<std::string>( "lib" ) );
+MadGraphProcessBuilder::MadGraphProcessBuilder(const ParametersList& params)
+    :  //KTProcess( params, std::array<pdgid_t,2>{}, std::vector<pdgid_t>{} )
+      Process2to4(params, std::array<pdgid_t, 2>{}, 0) {
+  if (params.has<std::string>("lib"))
+    loadLibrary(params.get<std::string>("lib"));
   else {
-    const MadGraphInterface interf( params );
-    loadLibrary( interf.run() );
+    const MadGraphInterface interf(params);
+    loadLibrary(interf.run());
   }
   //--- once MadGraph process library is loaded into runtime environment
   //    can define its wrapper object
-  mg5_proc_.reset( new MadGraphProcess );
+  mg5_proc_.reset(new MadGraphProcess);
 }
 
 void
 //MadGraphProcessBuilder::preparePhaseSpace()
-MadGraphProcessBuilder::prepareProcessKinematics()
-{
-  if ( !mg5_proc_ )
-    CG_FATAL( "MadGraphProcessBuilder" ) << "Process not properly linked!";
+MadGraphProcessBuilder::prepareProcessKinematics() {
+  if (!mg5_proc_)
+    CG_FATAL("MadGraphProcessBuilder") << "Process not properly linked!";
 
-  mg5_proc_->initialise( params_.get<std::string>( "parametersCard", "param_card.dat" ) );
+  mg5_proc_->initialise(params_.get<std::string>("parametersCard", "param_card.dat"));
 }
 
 /*void
@@ -67,10 +62,9 @@ MadGraphProcessBuilder::fillCentralParticlesKinematics()
 
 double
 //MadGraphProcessBuilder::computeKTFactorisedMatrixElement()
-MadGraphProcessBuilder::computeCentralMatrixElement() const
-{
-  if ( !mg5_proc_ )
-    CG_FATAL( "MadGraphProcessBuilder" ) << "Process not properly linked!";
+MadGraphProcessBuilder::computeCentralMatrixElement() const {
+  if (!mg5_proc_)
+    CG_FATAL("MadGraphProcessBuilder") << "Process not properly linked!";
 
   //CG_INFO("")<<qt1_*qt1_<<"::"<<t1_<<"\t"<<qt2_*qt2_<<"::"<<t2_;
 
@@ -79,15 +73,15 @@ MadGraphProcessBuilder::computeCentralMatrixElement() const
   q1.setPz( std::sqrt( t1_-qt1_*qt1_ ) );
   q1.setMass( mg5_proc_->masses()[0] );*/
   //auto q1 = Momentum::fromPtEtaPhiM( qt1_, 0., phi_qt1_, mg5_proc_->masses()[0] );
-  mg5_proc_->setMomentum( 0, q1_ );
+  mg5_proc_->setMomentum(0, q1_);
   // second incoming parton
   /*auto q2 = Momentum::fromPtEtaPhi( qt2_, 0., phi_qt2_ );
   q2.setPz( std::sqrt( t2_-qt2_*qt2_ ) );
   q2.setMass( mg5_proc_->masses()[1] );*/
   //auto q2 = Momentum::fromPtEtaPhiM( qt2_, 0., phi_qt2_, mg5_proc_->masses()[1] );
-  mg5_proc_->setMomentum( 1, q2_ );
+  mg5_proc_->setMomentum(1, q2_);
 
   return mg5_proc_->eval();
 }
 
-REGISTER_PROCESS( "mg5_aMC", MadGraphProcessBuilder )
+REGISTER_PROCESS("mg5_aMC", MadGraphProcessBuilder)
