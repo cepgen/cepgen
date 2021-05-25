fa90d97c84a5d849b970c1d31f4a69f79dc67bc5
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-05-25 17:41:28 +0200

    Filled LHAPDF comparison test

--- test/test_lhapdf_collflux.cxx
+++ test/test_lhapdf_collflux.cxx
@@ -1,21 +1,67 @@
+#include "CepGen/Physics/CollinearFlux.h"
+#include "CepGen/Physics/KTFlux.h"
+#include "CepGen/Generator.h"
+
+#include "CepGen/Modules/StructureFunctionsFactory.h"
+#include "CepGen/StructureFunctions/Parameterisation.h"
+#include "CepGen/FormFactors/Parameterisation.h"
+
 #include "CepGen/Utils/ArgumentsParser.h"
+#include "Canvas.h"
 
 #include <LHAPDF/LHAPDF.h>
+#include "TGraph.h"
+#include "TMultiGraph.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  double q2;
-  string set;
-  int member;
+  double q2, xmin, xmax;
+  string ffmode, set;
+  int strfun_type, member, num_points;
   cepgen::ArgumentsParser(argc, argv)
       .addOptionalArgument("q2", "Virtuality", &q2, 100.)
+      .addOptionalArgument("xmin,x", "minimal fractional loss", &xmin, 1.e-5)
+      .addOptionalArgument("xmax,X", "maximal fractional loss", &xmax, 1.)
+      .addOptionalArgument("formfac,f", "form factors modelling", &ffmode, "StandardDipole")
+      .addOptionalArgument("sf,s", "structure functions modelling", &strfun_type, 301)
       .addOptionalArgument("set,s", "PDFset to use", &set, "LUXqed17_plus_PDF4LHC15_nnlo_100")
       .addOptionalArgument("member,m", "PDF member", &member, 0)
+      .addOptionalArgument("num-points,n", "Number of points to probe", &num_points, 100)
       .parse();
 
-  const LHAPDF::PDF* pdf = LHAPDF::mkPDF(set, member);
-  double x = 0.;
-  const double xfx = pdf->xfxQ2(22, x, q2);
+  cepgen::initialise();
+
+  unique_ptr<LHAPDF::PDF> pdf(LHAPDF::mkPDF(set, member));
+
+  auto sf = cepgen::strfun::StructureFunctionsFactory::get().build(strfun_type);
+  auto ff = cepgen::formfac::FormFactorsFactory::get().build(ffmode);
+  ff->setStructureFunctions(sf.get());
+
+  cepgen::CollinearFlux flux(cepgen::Limits(0., 10000.), ff.get());
+
+  TGraph g_ref, g_cg, g_ratio;
+  for (int i = 0; i < num_points; ++i) {
+    const double x = xmin + i * (xmax - xmin) / (num_points + 1);
+    const double xfx = pdf->xfxQ2(22, x, q2);
+    const double pdf = flux(x, 0.938, cepgen::KTFlux::P_Photon_Elastic_Budnev);
+    cout << x << "\t" << xfx << "\t" << pdf << "\t" << pdf / xfx << endl;
+    g_ref.SetPoint(g_ref.GetN(), x, xfx);
+    g_cg.SetPoint(g_cg.GetN(), x, pdf);
+    g_ratio.SetPoint(g_ratio.GetN(), x, pdf / xfx);
+  }
+
+  cepgen::Canvas c(argv[0]);
+  TMultiGraph mg;
+  g_ref.SetLineColor(kRed + 1);
+  g_cg.SetLineColor(kBlue + 2);
+  //mg.Add(&g_ref);
+  //mg.Add(&g_cg);
+  mg.Add(&g_ratio);
+  mg.SetMinimum(1.e-10);
+  mg.Draw("al");
+  c.Prettify(mg.GetHistogram());
+  c.SetLogy();
+  c.Save("pdf");
   return 0;
 }
