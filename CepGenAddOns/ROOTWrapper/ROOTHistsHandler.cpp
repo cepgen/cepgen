5550dfe9e2c403fdb552e9214d9cb24a28df99f1
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-08-03 11:50:31 +0200

    ROOT: added GPLv3 license boilerplate

--- CepGenAddOns/ROOTWrapper/ROOTHistsHandler.cpp
+++ CepGenAddOns/ROOTWrapper/ROOTHistsHandler.cpp
@@ -1,22 +1,37 @@
-#include "CepGen/Core/ExportModule.h"
-#include "CepGen/Modules/ExportModuleFactory.h"
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2021  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <TFile.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TProfile.h>
+#include <TProfile2D.h>
 
 #include "CepGen/Core/Exception.h"
-
+#include "CepGen/Core/ExportModule.h"
 #include "CepGen/Event/Event.h"
 #include "CepGen/Event/EventBrowser.h"
+#include "CepGen/Modules/ExportModuleFactory.h"
 #include "CepGen/Parameters.h"
-
-#include "CepGen/Utils/String.h"
 #include "CepGen/Utils/Limits.h"
+#include "CepGen/Utils/String.h"
 #include "CepGen/Version.h"
-
-#include "TFile.h"
-#include "TH1.h"
-#include "TH2.h"
-#include "TH3.h"
-#include "TProfile.h"
-#include "TProfile2D.h"
 
 namespace cepgen {
   namespace io {
--- CepGenAddOns/ROOTWrapper/ROOTTreeInfo.cpp
+++ CepGenAddOns/ROOTWrapper/ROOTTreeInfo.cpp
@@ -1,10 +1,81 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2021  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CepGen/Core/Exception.h"
+#include "CepGen/Event/Event.h"
 #include "CepGenAddOns/ROOTWrapper/ROOTTreeInfo.h"
 
-#include "CepGen/Event/Event.h"
-#include "CepGen/Core/Exception.h"
-
 namespace ROOT {
+  CepGenRun::CepGenRun() { clear(); }
+
+  void CepGenRun::clear() {
+    sqrt_s = -1.;
+    xsect = errxsect = -1.;
+    num_events = litigious_events = 0;
+  }
+
+  void CepGenRun::create() {
+    tree_ = std::make_shared<TTree>(TREE_NAME, "a tree containing information on the previous run");
+    if (!tree_)
+      throw std::runtime_error("Failed to create the run TTree!");
+    tree_->Branch("xsect", &xsect, "xsect/D");
+    tree_->Branch("errxsect", &errxsect, "errxsect/D");
+    tree_->Branch("num_events", &num_events, "num_events/i");
+    tree_->Branch("litigious_events", &litigious_events, "litigious_events/i");
+    tree_->Branch("sqrt_s", &sqrt_s, "sqrt_s/D");
+  }
+
+  void CepGenEvent::clear() {
+    gen_time = tot_time = 0.;
+    np = 0;
+    for (size_t i = 0; i < MAX_PART; ++i) {
+      pt[i] = eta[i] = phi[i] = rapidity[i] = E[i] = m[i] = charge[i] = 0.;
+      pdg_id[i] = parent1[i] = parent2[i] = stable[i] = role[i] = status[i] = 0;
+    }
+  }
+
+  void CepGenEvent::create() {
+    tree_ = std::make_shared<TTree>(TREE_NAME, "a tree containing information on events generated in previous run");
+    if (!tree_)
+      throw CG_FATAL("CepGenEvent:create") << "Failed to create the events TTree!";
+    tree_->Branch("npart", &np, "npart/I");
+    tree_->Branch("role", role, "role[npart]/I");
+    tree_->Branch("pt", pt, "pt[npart]/D");
+    tree_->Branch("eta", eta, "eta[npart]/D");
+    tree_->Branch("phi", phi, "phi[npart]/D");
+    tree_->Branch("rapidity", rapidity, "rapidity[npart]/D");
+    tree_->Branch("E", E, "E[npart]/D");
+    tree_->Branch("m", m, "m[npart]/D");
+    tree_->Branch("charge", charge, "charge[npart]/D");
+    tree_->Branch("pdg_id", pdg_id, "pdg_id[npart]/I");
+    tree_->Branch("parent1", parent1, "parent1[npart]/I");
+    tree_->Branch("parent2", parent2, "parent2[npart]/I");
+    tree_->Branch("stable", stable, "stable[npart]/I");
+    tree_->Branch("status", status, "status[npart]/I");
+    tree_->Branch("weight", &weight, "weight/F");
+    tree_->Branch("generation_time", &gen_time, "generation_time/F");
+    tree_->Branch("total_time", &tot_time, "total_time/F");
+  }
+
   void CepGenEvent::fill(const cepgen::Event& ev, bool compress) {
+    if (!tree_)
+      throw CG_FATAL("CepGenEvent:fill") << "Trying to fill a non-existent tree!";
+
     clear();
     gen_time = ev.time_generation;
     tot_time = ev.time_total;
@@ -29,7 +100,8 @@ namespace ROOT {
       role[np] = part.role();
       np++;
     }
-    fill();
+    tree_->Fill();
+    clear();
   }
 
   bool CepGenEvent::next(cepgen::Event& ev) {
@@ -37,7 +109,7 @@ namespace ROOT {
       try {
         attach();
       } catch (const std::runtime_error& err) {
-        throw CG_FATAL("ROOT::CepGenEvent") << "Failed to attach to the events TTree!\n" << err.what();
+        throw CG_FATAL("CepGenEvent:next") << "Failed to attach to the events TTree!\n" << err.what();
       }
 
     if (tree_->GetEntry(num_read_events_++) <= 0)
