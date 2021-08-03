d08071f9bd8ebb071d21ba4a8b935f9c04dd5b2f
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-08-03 11:50:06 +0200

    Modules+Cards+Integration: added GPLv3 license boilerplate

--- CepGen/Integration/GridParameters.h
+++ CepGen/Integration/GridParameters.h
@@ -1,5 +1,23 @@
-#ifndef CepGen_Core_GridParameters_h
-#define CepGen_Core_GridParameters_h
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2021  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CepGen_Integration_GridParameters_h
+#define CepGen_Integration_GridParameters_h
 
 #include <cstddef>
 #include <vector>
---
+++ CepGen/Physics/AlphaEMSuperCHIC.cpp
@@ -0,0 +1,50 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2021  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CepGen/Physics/AlphaS.h"
+#include "CepGen/Physics/Constants.h"
+
+namespace cepgen {
+  class AlphaEMSuperCHIC final : public AlphaS {
+  public:
+    explicit AlphaEMSuperCHIC(const ParametersList& params) : AlphaS(params) {}
+    static std::string description() { return "SuperCHIC alphaEM evolution algorithm"; }
+
+    double operator()(double q) const override {
+      const double q2 = q * q;
+      if (q2 < 2.e-6)
+        return constants::ALPHA_EM;
+      const double log_q2 = log(q2), log_1_pl_q2 = log(1. + q2);
+      double rpigg;
+      if (q2 < 9.e-2)
+        rpigg = AEM_3PI * (13.4916 + log_q2) + 0.00835 * log_1_pl_q2;
+      else if (q2 < 9.)
+        rpigg = AEM_3PI * (16.32 + 2. * log_q2) + 0.00238 * log(1. + 3.927 * q2);
+      else if (q2 < 1.e4)
+        rpigg = AEM_3PI * (13.4955 + 3. * log_q2) + 0.00165 + 0.00299 * log_1_pl_q2;
+      else
+        rpigg = AEM_3PI * (13.4955 + 3. * log_q2) + 0.00221 + 0.00293 * log_1_pl_q2;
+      return constants::ALPHA_EM / (1. - rpigg);
+    }
+
+  private:
+    static constexpr double AEM_3PI = constants::ALPHA_EM / 3. * M_1_PI;
+  };
+}  // namespace cepgen
+
+REGISTER_ALPHAS_MODULE("superchic", AlphaEMSuperCHIC)
