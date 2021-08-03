686f7a006c9d5710d749bf6914a1bff66a1bdb48
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-08-03 23:25:05 +0200

    Utils: collinear LHAPDF flux now part of utils collection instead of tests

--- src/utils/plot_collinearflux_lhapdf.cxx
+++ src/utils/plot_collinearflux_lhapdf.cxx
@@ -1,17 +1,33 @@
-#include "CepGen/Physics/CollinearFlux.h"
-#include "CepGen/Physics/KTFlux.h"
-#include "CepGen/Generator.h"
-
-#include "CepGen/Modules/StructureFunctionsFactory.h"
-#include "CepGen/StructureFunctions/Parameterisation.h"
-#include "CepGen/FormFactors/Parameterisation.h"
-
-#include "CepGen/Utils/ArgumentsParser.h"
-#include "Canvas.h"
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2021  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <LHAPDF/LHAPDF.h>
-#include "TGraph.h"
-#include "TMultiGraph.h"
+#include <TGraph.h>
+#include <TMultiGraph.h>
+
+#include "CepGen/FormFactors/Parameterisation.h"
+#include "CepGen/Generator.h"
+#include "CepGen/Modules/StructureFunctionsFactory.h"
+#include "CepGen/Physics/CollinearFlux.h"
+#include "CepGen/Physics/KTFlux.h"
+#include "CepGen/StructureFunctions/Parameterisation.h"
+#include "CepGen/Utils/ArgumentsParser.h"
+#include "CepGenAddOns/ROOTWrapper/ROOTCanvas.h"
 
 using namespace std;
 
@@ -56,7 +72,7 @@ int main(int argc, char* argv[]) {
     g_ratio.SetPoint(g_ratio.GetN(), x, pdf / xfx);
   }
 
-  cepgen::Canvas c(output.c_str());
+  cepgen::ROOTCanvas c(output.c_str());
   TMultiGraph mg;
   g_ref.SetLineColor(kRed + 1);
   g_cg.SetLineColor(kBlue + 2);
