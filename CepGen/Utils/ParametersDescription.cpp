59bda96f0a53f8def9d3a66d1477cbad47d2040c
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-11-30 11:36:28 +0100

    Utils: added a parameters description framework

---
+++ CepGen/Utils/ParametersDescription.cpp
@@ -0,0 +1,54 @@
+#include <sstream>
+
+#include "CepGen/Core/Exception.h"
+#include "CepGen/Utils/ParametersDescription.h"
+#include "CepGen/Utils/String.h"
+
+namespace cepgen {
+  ParametersDescription::ParametersDescription(const std::string& mod_name) {
+    if (!mod_name.empty())
+      setName(mod_name);
+  }
+
+  std::string ParametersDescription::describe(size_t offset) const {
+    std::ostringstream os;
+    static auto sep = [](size_t offset) -> std::string { return std::string(offset, '\t'); };
+    const auto& mod_name = ParametersList::name<std::string>();
+    if (!mod_name.empty())
+      os << sep(offset) << "Name: " << utils::boldify(mod_name) << "\n";
+    for (const auto& key : ParametersList::keys(false)) {
+      os << sep(offset + 1) << utils::colourise(key, utils::Colour::reset, utils::Modifier::underline);
+      if (obj_descr_.count(key) > 0) {
+        const auto& obj = obj_descr_.at(key);
+        if (!ParametersList::has<ParametersList>(key))
+          os << " (default value: " << ParametersList::getString(key) << ")";
+        os << "\n";
+        if (!obj.description().empty())
+          os << sep(offset + 2) << utils::colourise(obj.description(), utils::Colour::reset, utils::Modifier::italic)
+             << "\n";
+        os << obj.describe(offset + 2);
+      }
+    }
+    return os.str();
+  }
+
+  ParametersDescription& ParametersDescription::setDescription(const std::string& descr) {
+    mod_descr_ = descr;
+    return *this;
+  }
+
+  template <>
+  ParametersDescription& ParametersDescription::add<ParametersDescription>(const std::string& name,
+                                                                           const ParametersDescription& desc) {
+    obj_descr_[name] = desc;
+    ParametersList::set<ParametersList>(name, desc.parameters());
+    return obj_descr_[name];
+  }
+
+  template <>
+  ParametersDescription& ParametersDescription::add<ParametersList>(const std::string& name, const ParametersList&) {
+    throw CG_FATAL("ParametersDescription:add")
+        << "Using a ParametersList object for the description of a collection of parameters is not allowed.\n"
+        << "Please use a ParametersDescription object instead for the description of the '" << name << "' collection.";
+  }
+}  // namespace cepgen
---
+++ CepGen/Utils/ParametersDescription.h
@@ -0,0 +1,41 @@
+#ifndef CepGen_Utils_ParametersDescription_h
+#define CepGen_Utils_ParametersDescription_h
+
+#include "CepGen/Core/ParametersList.h"
+
+namespace cepgen {
+  /// A description object for parameters collection
+  class ParametersDescription : private ParametersList {
+  public:
+    /// Build the description of a parameters collection object
+    /// \param[in] mod_name Module name (where applicable)
+    explicit ParametersDescription(const std::string& mod_name = "");
+    /// Set the description of this parameter (or parameters collection)
+    ParametersDescription& setDescription(const std::string& descr);
+    /// Description of this parameter (or parameters collection)
+    const std::string& description() const { return mod_descr_; }
+    /// Add the description to a new parameter
+    template <typename T>
+    ParametersDescription& add(const std::string& name, const T& def) {
+      obj_descr_[name] = ParametersDescription();
+      ParametersList::set<T>(name, def);
+      return obj_descr_[name];
+    }
+    /// Human-readable description of all parameters and their default value
+    std::string describe(size_t offset = 0) const;
+    /// List of parameters associated to this description object
+    const ParametersList& parameters() const { return *this; }
+
+  private:
+    std::string mod_descr_;
+    std::map<std::string, ParametersDescription> obj_descr_;
+  };
+  /// Add the description to a new sub-description (aka ParametersList) object
+  template <>
+  ParametersDescription& ParametersDescription::add(const std::string&, const ParametersDescription&);
+  /// Disable the addition of a ParametersList object to this description
+  template <>
+  ParametersDescription& ParametersDescription::add(const std::string&, const ParametersList&);
+}  // namespace cepgen
+
+#endif
