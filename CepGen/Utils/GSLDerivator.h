be1de9bf472331e27f41e8cb449baa10a70b0d3a
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-01-17 13:38:12 +0100

    Utils: simplified signature of GSL-wrapped functions

--- CepGen/Utils/GSLDerivator.h
+++ CepGen/Utils/GSLDerivator.h
@@ -1,3 +1,21 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2022  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CepGen_Utils_GSLDerivator_h
 #define CepGen_Utils_GSLDerivator_h
 
@@ -22,7 +40,7 @@ namespace cepgen {
       }
 
       /// A one-dimensional function to evaluate
-      typedef std::function<double(double, void*)> Function1D;
+      typedef std::function<double(double)> Function1D;
 
       /// Evaluate the derivative of a function at a given value
       /// \param[in] x coordinate
@@ -31,7 +49,7 @@ namespace cepgen {
         int res{GSL_SUCCESS};
         double val, val_unc;
         const double step_size = h > 0. ? h : h_;
-        auto gfunc = utils::GSLFunctionWrapper<decltype(func)>::build(func);
+        auto gfunc = utils::GSLFunctionWrapper::build(func);
         switch (mode_) {
           case Mode::central:
             res = gsl_deriv_central(gfunc.get(), x, step_size, &val, &val_unc);
--- CepGen/Utils/GSLFunctionsWrappers.h
+++ CepGen/Utils/GSLFunctionsWrappers.h
@@ -1,3 +1,21 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2022  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CepGen_Utils_GSLFunctionsWrappers_h
 #define CepGen_Utils_GSLFunctionsWrappers_h
 
@@ -7,24 +25,22 @@
 namespace cepgen {
   namespace utils {
     /// GSL wrapper to define a functor as a GSL-digestible functional
-    /// \tparam F functor member signature
-    template <typename F>
     class GSLFunctionWrapper : public gsl_function {
     public:
-      explicit GSLFunctionWrapper(const F& func) : func_(func) {
+      explicit GSLFunctionWrapper(const std::function<double(double)>& func) : func_(func) {
         function = &GSLFunctionWrapper::eval;
         params = this;
       }
       /// Utility to build a gsl_function pointer from a functional
-      static std::unique_ptr<gsl_function> build(const F& func) {
-        return std::unique_ptr<gsl_function>(new utils::GSLFunctionWrapper<decltype(func)>(func));
+      static std::unique_ptr<gsl_function> build(const std::function<double(double)>& func) {
+        return std::unique_ptr<gsl_function>(new utils::GSLFunctionWrapper(func));
       }
 
     private:
       /// Static integrable functional
-      static double eval(double x, void* params) { return static_cast<GSLFunctionWrapper*>(params)->func_(x, params); }
+      static double eval(double x, void* params) { return static_cast<GSLFunctionWrapper*>(params)->func_(x); }
       /// Reference to the functor
-      const F& func_;
+      const std::function<double(double)>& func_;
     };
 
     /// GSL wrapper to define a functor as an integrable functional
