175caecf8c3a2d02bca76e38bf7d50f9e5d7b3ee
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-01-23 23:09:43 +0100

    Utils: form factors plotter generalised

--- src/utils/cepgenPlotFormFactors.cc
+++ src/utils/cepgenPlotFormFactors.cc
@@ -16,42 +16,42 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <TGraph.h>
-#include <TMultiGraph.h>
-
 #include <fstream>
 
 #include "CepGen/Core/Exception.h"
 #include "CepGen/FormFactors/Parameterisation.h"
 #include "CepGen/Generator.h"
+#include "CepGen/Modules/DrawerFactory.h"
 #include "CepGen/Modules/StructureFunctionsFactory.h"
-#include "CepGen/Physics/PDG.h"
 #include "CepGen/StructureFunctions/Parameterisation.h"
 #include "CepGen/Utils/ArgumentsParser.h"
-#include "CepGenAddOns/ROOTWrapper/ROOTCanvas.h"
+#include "CepGen/Utils/Drawer.h"
+#include "CepGen/Utils/Graph.h"
+#include "CepGen/Utils/String.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
   int mode, strfun_type, num_points;
   double mx, q2min, q2max;
-  string output_file;
-  bool draw_grid;
+  string output_file, plotter;
+  bool logy, draw_grid;
 
   cepgen::ArgumentsParser(argc, argv)
       .addArgument("mode,t", "beam modelling", &mode, (int)cepgen::Beam::Mode::ProtonElastic)
       .addOptionalArgument("mx,x", "diffractive mass (GeV/c^2)", &mx, 100.)
-      .addOptionalArgument("sf,s", "structure functions modelling", &strfun_type, 301)
+      .addOptionalArgument("sf,s", "structure functions modelling", &strfun_type, 11)
       .addOptionalArgument("q2min,m", "minimal parton virtuality (GeV^2)", &q2min, 1.)
-      .addOptionalArgument("q2max,M", "maximal parton virtuality (GeV^2)", &q2max, 10000.)
+      .addOptionalArgument("q2max,M", "maximal parton virtuality (GeV^2)", &q2max, 2.5)
       .addOptionalArgument("npoints,n", "number of x-points to scan", &num_points, 500)
       .addOptionalArgument("output,o", "output file name", &output_file, "formfacs.scan.output.txt")
+      .addOptionalArgument("logy,l", "logarithmic y-axis", &logy, false)
       .addOptionalArgument("draw-grid,g", "draw the x/y grid", &draw_grid, false)
+      .addOptionalArgument("plotter,p", "type of plotter to user", &plotter, "root")
       .parse();
 
   cepgen::initialise();
-  //const double mi = cepgen::PDG::get().mass( cepgen::PDG::proton );
-  //const double mi2 = mi*mi, mx2 = mx*mx;
+  auto plt = cepgen::utils::DrawerFactory::get().build(plotter);
 
   ofstream out(output_file);
   out << "# form factors: ";
@@ -65,13 +65,12 @@ int main(int argc, char* argv[]) {
       << "# q2 in [" << q2min << ", " << q2max << "] GeV^2\n";
 
   vector<unique_ptr<cepgen::formfac::Parameterisation> > form_factors;
-  vector<TGraph*> g_form_factors_fe, g_form_factors_fm;
+  vector<cepgen::utils::Graph1D> g_form_factors_fe, g_form_factors_fm;
   for (const auto& ff_type : cepgen::formfac::FormFactorsFactory::get().modules()) {
     form_factors.emplace_back(cepgen::formfac::FormFactorsFactory::get().build(ff_type));
-    g_form_factors_fe.emplace_back(new TGraph);
-    (*g_form_factors_fe.rbegin())->SetTitle((ff_type + ";Q^{2} (GeV^{2});F_{E}").c_str());
-    g_form_factors_fm.emplace_back(new TGraph);
-    (*g_form_factors_fm.rbegin())->SetTitle((ff_type + ";Q^{2} (GeV^{2});F_{M}").c_str());
+    const auto ff_desc = cepgen::formfac::FormFactorsFactory::get().describe(ff_type);
+    g_form_factors_fe.emplace_back("fe_" + ff_type, ff_desc);
+    g_form_factors_fm.emplace_back("fm_" + ff_type, ff_desc);
   }
   for (int i = 0; i < num_points; ++i) {
     const double q2 = q2min + i * (q2max - q2min) / (num_points - 1);
@@ -80,8 +79,8 @@ int main(int argc, char* argv[]) {
     for (auto& ff : form_factors) {
       const auto form_factor = (*ff)((cepgen::Beam::Mode)mode, q2, mx, sf.get());
       out << "\t" << form_factor.FE << "\t" << form_factor.FM;
-      g_form_factors_fe.at(j)->SetPoint(g_form_factors_fe.at(j)->GetN(), q2, form_factor.FE);
-      g_form_factors_fm.at(j)->SetPoint(g_form_factors_fm.at(j)->GetN(), q2, form_factor.FM);
+      g_form_factors_fe.at(j).addPoint(q2, form_factor.FE);
+      g_form_factors_fm.at(j).addPoint(q2, form_factor.FM);
       ++j;
     }
     out << "\n";
@@ -89,25 +88,21 @@ int main(int argc, char* argv[]) {
   CG_LOG << "Scan written in \"" << output_file << "\".";
   out.close();
 
-  for (auto& plt : map<const char*, vector<TGraph*> >{{"FE", g_form_factors_fe}, {"FM", g_form_factors_fm}}) {
-    cepgen::ROOTCanvas c(plt.first, Form("M_{X} = %g GeV/c^{2}", mx));
-    c.SetLogy();
-    if (draw_grid)
-      c.SetGrid(true, true);
-    TMultiGraph mg;
-    size_t i = 0;
-    for (auto& gr : plt.second) {
-      mg.Add(gr);
-      gr->SetLineColor(cepgen::ROOTCanvas::colours[i]);
-      c.AddLegendEntry(gr, gr->GetTitle(), "l");
-      ++i;
+  cepgen::utils::Drawer::Mode dm;
+  if (logy)
+    dm |= cepgen::utils::Drawer::Mode::logy;
+  if (draw_grid)
+    dm |= cepgen::utils::Drawer::Mode::grid;
+
+  for (auto& canv : map<pair<string, string>, vector<cepgen::utils::Graph1D> >{{{"fe", "F_{E}"}, g_form_factors_fe},
+                                                                               {{"fm", "F_{M}"}, g_form_factors_fm}}) {
+    cepgen::utils::DrawableColl mp;
+    for (auto& gr : canv.second) {
+      gr.xAxis().setLabel("Q^{2} (GeV^{2})");
+      gr.yAxis().setLabel(canv.first.second);
+      mp.emplace_back(&gr);
     }
-    mg.Draw("al");
-    // ugly fix to propagate first plot axes label onto multigraph
-    mg.GetHistogram()->GetXaxis()->SetTitle((*plt.second.begin())->GetXaxis()->GetTitle());
-    mg.GetHistogram()->GetYaxis()->SetTitle((*plt.second.begin())->GetYaxis()->GetTitle());
-    c.Prettify(mg.GetHistogram());
-    c.Save("pdf");
+    plt->draw(mp, "comp_" + canv.first.first, cepgen::utils::format("M_{X} = %g GeV/c^{2}", mx), dm);
   }
 
   return 0;
