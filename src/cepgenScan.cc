24b9c729e6c0c536f4645429513bfbddda4dd4a4
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-01-24 01:38:14 +0100

    Utils: scanner has now a plot producer

--- src/cepgenScan.cc
+++ src/cepgenScan.cc
@@ -23,30 +23,37 @@
 #include "CepGen/Core/ExportModule.h"
 #include "CepGen/Generator.h"
 #include "CepGen/Modules/CardsHandlerFactory.h"
+#include "CepGen/Modules/DrawerFactory.h"
 #include "CepGen/Parameters.h"
 #include "CepGen/Physics/PDG.h"
 #include "CepGen/Processes/Process.h"
 #include "CepGen/Utils/AbortHandler.h"
 #include "CepGen/Utils/ArgumentsParser.h"
+#include "CepGen/Utils/Drawer.h"
+#include "CepGen/Utils/Graph.h"
 #include "CepGen/Utils/Logger.h"
 #include "CepGen/Utils/String.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  string input_config, output_file, scan;
+  string input_config, output_file, scan, plotter;
   int npoints;
   double min_value, max_value;
   vector<double> points;
+  bool draw_grid, logy;
 
   cepgen::ArgumentsParser parser(argc, argv);
   parser.addArgument("config,i", "base configuration", &input_config)
       .addOptionalArgument("scan,s", "type of scan to perform", &scan, "ptmin")
-      .addOptionalArgument("min,l", "minimum value of scan", &min_value, 1.)
-      .addOptionalArgument("max,H", "maximum value of scan", &max_value, 11.)
+      .addOptionalArgument("min,m", "minimum value of scan", &min_value, 1.)
+      .addOptionalArgument("max,M", "maximum value of scan", &max_value, 11.)
       .addOptionalArgument("num-points,n", "number of points to consider", &npoints, 10)
-      .addOptionalArgument("points,p", "list of points to consider", &points, vector<double>{})
+      .addOptionalArgument("points", "list of points to consider", &points, vector<double>{})
       .addOptionalArgument("output,o", "output file", &output_file, "xsect.dat")
+      .addOptionalArgument("logy,l", "logarithmic y-scale", &logy, false)
+      .addOptionalArgument("draw-grid,g", "draw the x/y grid", &draw_grid, false)
+      .addOptionalArgument("plotter,p", "type of plotter to user", &plotter, "")
       .parse();
 
   cepgen::Generator mg;
@@ -75,23 +82,30 @@ int main(int argc, char* argv[]) {
 
   cepgen::utils::AbortHandler();
 
+  cepgen::utils::Graph1D graph;
+
   auto& kin = par.process().kinematics();
   double cross_section, err_cross_section;
+  string scan_str = scan;
   for (const auto& value : points) {
     try {
       if (scan == "sqrtS") {
         kin.incomingBeams().setSqrtS(value);
+        scan_str = "#sqrt{s} (GeV)";
       } else if (scan == "abseta") {
         kin.cuts().central.eta_single().min() = -value;
         kin.cuts().central.eta_single().max() = +value;
+        scan_str = "|#eta|";
       } else if (scan == "absrap") {
         kin.cuts().central.rapidity_single().min() = -value;
         kin.cuts().central.rapidity_single().max() = +value;
+        scan_str = "|y|";
       } else if (scan == "mpart") {
         auto prop = cepgen::PDG::get()(par.process().event()[cepgen::Particle::CentralSystem][0].pdgId());
         prop.mass = value;
         cepgen::PDG::get().define(prop);
         par.process().clear();
+        scan_str = "m_{central}^{single}";
       } else {
         auto modif = cepgen::ParametersList().set<double>(scan, value);
         kin.setParameters(modif);
@@ -100,6 +114,7 @@ int main(int argc, char* argv[]) {
       CG_LOG << "Scan of \"" << scan << "\". Value = " << value << ".";
       mg.computeXsection(cross_section, err_cross_section);
       string out_line = cepgen::utils::format("%.2f\t%.8e\t%.8e\n", value, cross_section, err_cross_section);
+      graph.addPoint(value, cross_section, 0., err_cross_section);
       xsect_file << out_line;
       CG_LOG << out_line;
       xsect_file.flush();
@@ -107,6 +122,18 @@ int main(int argc, char* argv[]) {
       CG_LOG << "Run aborted!";
       break;
     }
+  }
+
+  if (!plotter.empty()) {
+    cepgen::utils::Drawer::Mode dm;
+    if (logy)
+      dm |= cepgen::utils::Drawer::Mode::logy;
+    if (draw_grid)
+      dm |= cepgen::utils::Drawer::Mode::grid;
+    graph.xAxis().setLabel(scan_str);
+    graph.yAxis().setLabel("#sigma_{gen} (pb)");
+    auto plt = cepgen::utils::DrawerFactory::get().build(plotter);
+    plt->draw(graph, dm);
   }
 
   return 0;
