47343d2ef5bdb12b5057b14d75f5c044bd58d879
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-02-09 00:43:43 +0100

    Python: started to fill the functional builder ; first moving of files in the package

--- CepGenAddOns/PythonWrapper/FunctionalPython.cpp
+++ CepGenAddOns/PythonWrapper/FunctionalPython.cpp
@@ -18,6 +18,7 @@
 
 #include <Python.h>
 
+#include "CepGen/Core/Exception.h"
 #include "CepGen/Modules/FunctionalFactory.h"
 #include "CepGen/Utils/Functional.h"
 
@@ -26,17 +27,71 @@ namespace cepgen {
     class FunctionalPython final : public Functional {
     public:
       explicit FunctionalPython(const ParametersList&);
+      ~FunctionalPython();
 
       static ParametersDescription description();
 
       double eval(const std::vector<double>&) const;
 
     private:
+      mutable PyObject* value_;
+      PyObject* func_;
+      PyObject* mod_;
     };
 
-    FunctionalPython::FunctionalPython(const ParametersList& params) : Functional(params) {}
+    FunctionalPython::FunctionalPython(const ParametersList& params) : Functional(params) {
+      Py_Initialize();
+      auto* global = PyDict_New();
+      mod_ = PyModule_New("functional");
+      PyModule_AddStringConstant(mod_, "__file__", "");
+      auto* local = PyModule_GetDict(mod_);
+      std::ostringstream os;
+      os << "from math import *\n"
+         << "def custom_functional(";
+      std::string sep;
+      for (const auto& var : vars_)
+        os << sep << var, sep = ", ";
+      os << "):\n"
+         << "\treturn " << expression_ << "\n";
+      CG_LOG << os.str();
+      value_ = PyRun_String(os.str().c_str(), Py_file_input, global, local);
+      if (!value_) {
+        if (PyErr_Occurred()) {
+        }
+        throw CG_ERROR("FunctionalPython")
+            << "Failed to initialise the Python functional with \"" << expression_ << "\".";
+      }
+      Py_DECREF(value_);
+      func_ = PyObject_GetAttrString(mod_, "custom_functional");
+      if (!func_ || !PyCallable_Check(func_)) {
+        if (PyErr_Occurred()) {
+          PyErr_Print();
+        }
+        throw CG_ERROR("FunctionalPython") << "Failed to retrieve/cast the object to a Python functional.";
+      }
+    }
 
-    double FunctionalPython::eval(const std::vector<double>& x) const { return 0.; }
+    FunctionalPython::~FunctionalPython() {
+      Py_DECREF(value_);
+      Py_XDECREF(func_);
+      Py_DECREF(mod_);
+      Py_Finalize();
+    }
+
+    double FunctionalPython::eval(const std::vector<double>& x) const {
+      auto* args = PyTuple_New(x.size());
+      for (size_t i = 0; i < x.size(); ++i) {
+        value_ = PyFloat_FromDouble(x.at(i));
+        PyTuple_SetItem(args, i, value_);
+      }
+      value_ = PyObject_CallObject(func_, args);
+      Py_DECREF(args);
+      //if (!PyFloat_Check(value_))
+      //  throw CG_ERROR("FunctionalPython") << "Return object is not a float.";
+      auto ret = PyFloat_AsDouble(value_);
+      CG_LOG << "ret:" << ret;
+      return ret;
+    }
 
     ParametersDescription FunctionalPython::description() {
       auto desc = Functional::description();
