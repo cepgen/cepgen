cf5a18fa581da36a1a43e6839f64b67118f4e2a2
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-02-10 09:58:32 +0100

    Python: safer initialisation of globals

--- CepGenAddOns/PythonWrapper/FunctionalPython.cpp
+++ CepGenAddOns/PythonWrapper/FunctionalPython.cpp
@@ -35,37 +35,40 @@ namespace cepgen {
       double eval(const std::vector<double>&) const;
 
     private:
-      mutable PyObject* value_;
       PyObject* func_;
       PyObject* mod_;
     };
 
     FunctionalPython::FunctionalPython(const ParametersList& params) : Functional(params) {
       Py_Initialize();
-      auto* global = PyDict_New();
-      mod_ = PyModule_New("functional");
+      auto* global = PyEval_GetGlobals();  // borrowed
+      //auto* math = PyImport_ImportModule("math");
+      //PyDict_Merge(global, PyModule_GetDict(math), true);
+      mod_ = PyModule_New("functional");  // new
       PyModule_AddStringConstant(mod_, "__file__", "");
-      auto* local = PyModule_GetDict(mod_);
+      auto* local = PyModule_GetDict(mod_);  // borrowed
       std::ostringstream os;
-      os << "from math import *\n"
-         << "def custom_functional(";
+      os  //<< "from math import *\n"
+          << "def custom_functional(";
       std::string sep;
       for (const auto& var : vars_)
         os << sep << var, sep = ", ";
       os << "):\n"
          << "\treturn " << expression_ << "\n";
       CG_LOG << os.str();
-      value_ = PyRun_String(os.str().c_str(), Py_file_input, global, local);
-      if (!value_)
-        python::error("Failed to initialise the Python functional with \"" + expression_ + "\".");
-      Py_DECREF(value_);
+      {
+        auto* value = PyRun_String(os.str().c_str(), Py_file_input, global, local);  // new
+        //PyRun_SimpleString(os.str().c_str());
+        if (!value)
+          python::error("Failed to initialise the Python functional with \"" + expression_ + "\".");
+        Py_DECREF(value);
+      }
       func_ = PyObject_GetAttrString(mod_, "custom_functional");
       if (!func_ || !PyCallable_Check(func_))
         python::error("Failed to retrieve/cast the object to a Python functional.");
     }
 
     FunctionalPython::~FunctionalPython() {
-      Py_DECREF(value_);
       Py_XDECREF(func_);
       Py_DECREF(mod_);
       Py_Finalize();
@@ -73,15 +76,14 @@ namespace cepgen {
 
     double FunctionalPython::eval(const std::vector<double>& x) const {
       auto* args = PyTuple_New(x.size());
+      PyObject* value;
       for (size_t i = 0; i < x.size(); ++i) {
-        value_ = PyFloat_FromDouble(x.at(i));
-        PyTuple_SetItem(args, i, value_);
+        value = PyFloat_FromDouble(x.at(i));
+        PyTuple_SetItem(args, i, value);
       }
-      value_ = PyObject_CallObject(func_, args);
+      value = PyObject_CallObject(func_, args);
       Py_DECREF(args);
-      //if (!PyFloat_Check(value_))
-      //  throw CG_ERROR("FunctionalPython") << "Return object is not a float.";
-      auto ret = PyFloat_AsDouble(value_);
+      auto ret = python::get<double>(value);
       CG_LOG << "ret:" << ret;
       return ret;
     }
