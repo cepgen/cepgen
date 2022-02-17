9b6504586ef73541a4f3b1129366392bf3fd2f96
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-02-17 11:26:15 +0100

    Test: simplified functional parsers test flow

--- test/test_function_parser.cc
+++ test/test_function_parser.cc
@@ -29,15 +29,19 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  cepgen::ArgumentsParser(argc, argv).parse();
+  vector<string> parsers;
+  cepgen::ArgumentsParser(argc, argv)
+      .addOptionalArgument("parsers,p", "list of parsers to use", &parsers, vector<string>{})
+      .parse();
 
   const double epsilon = 1.e-9;  // tolerance
   cepgen::initialise();
 
-  const auto funcs = cepgen::utils::FunctionalFactory::get().modules();
-  CG_LOG << "Will test with " << cepgen::utils::s("module", funcs.size(), true) << ": " << funcs;
+  if (parsers.empty())
+    parsers = cepgen::utils::FunctionalFactory::get().modules();
+  CG_LOG << "Will test with " << cepgen::utils::s("module", parsers.size(), true) << ": " << parsers;
 
-  for (const auto& func : funcs) {
+  for (const auto& func : parsers) {
     CG_LOG << "Testing with \"" << func << "\" functional parser.";
     {  // test with a 1-variable function
       const double exp_result_test1 = 6.795704571;
@@ -45,51 +49,63 @@ int main(int argc, char* argv[]) {
                         .set<std::string>("expression", "2.5*exp(0.1*x)")
                         .set<std::vector<std::string> >("variables", {"x"});
       auto test = cepgen::utils::FunctionalFactory::get().build(func, params);
-      if (fabs((*test)(10.) - exp_result_test1) > epsilon)
-        throw CG_FATAL("main") << "Test 1.1 failed!";
-      if (fabs((*test)({10.}) - exp_result_test1) > epsilon)
-        throw CG_FATAL("main") << "Test 1.2 failed!";
-      CG_LOG << "Test 1 passed!";
+      try {
+        if (fabs((*test)(10.) - exp_result_test1) > epsilon) {
+          CG_LOG << "Test 1.1 failed.";
+          return -1;
+        }
+        if (fabs((*test)({10.}) - exp_result_test1) > epsilon) {
+          CG_LOG << "Test 1.2 failed.";
+          return -1;
+        }
+      } catch (const cepgen::Exception& e) {
+        CG_LOG << "Test 1 failed.";
+        return -1;
+      }
+      CG_LOG << "Test 1 passed.";
     }
     {  // test with an invalid function
-      bool passed = false;
+      auto params = cepgen::ParametersList()
+                        .set<std::string>("expression", "sqrt(x+x**3-log(10)")
+                        .set<std::vector<std::string> >("variables", {"x"});
       try {
-        auto params = cepgen::ParametersList()
-                          .set<std::string>("expression", "sqrt(x+x**3-log(10)")
-                          .set<std::vector<std::string> >("variables", {"x"});
         auto test = cepgen::utils::FunctionalFactory::get().build(func, params);
         (*test)(10);
+        CG_LOG << "Test 2 failed.";
+        return -1;
       } catch (const cepgen::Exception& e) {
-        CG_LOG << "Test 2 passed!";
-        passed = true;
+        CG_LOG << "Test 2 passed.";
       }
-      if (!passed)
-        throw CG_FATAL("main") << "Test 2 failed!";
     }
     {  // test with a 2-variables function
       auto params = cepgen::ParametersList()
                         .set<std::string>("expression", "sqrt(a^2+b^2)")
                         .set<std::vector<std::string> >("variables", {"a", "b"});
-      auto test = cepgen::utils::FunctionalFactory::get().build(func, params);
-      if (fabs((*test)({3, 4}) - 5.0) > epsilon)
-        throw CG_FATAL("main") << "Test 3 failed!";
-      CG_LOG << "Test 3 passed!";
+      try {
+        auto test = cepgen::utils::FunctionalFactory::get().build(func, params);
+        if (fabs((*test)({3, 4}) - 5.0) > epsilon) {
+          throw CG_ERROR("main") << "Exceeded the numerical limit.";
+          return -1;
+        }
+        CG_LOG << "Test 3 passed.";
+      } catch (const cepgen::Exception&) {
+        CG_LOG << "Test 3 failed.";
+        return -1;
+      }
     }
     {  // test with an invalid function
-      bool passed = false;
+      auto params = cepgen::ParametersList()
+                        .set<std::string>("expression", "a***2")
+                        .set<std::vector<std::string> >("variables", {"a"});
       try {
-        auto params = cepgen::ParametersList()
-                          .set<std::string>("expression", "a***2")
-                          .set<std::vector<std::string> >("variables", {"a"});
         auto test = cepgen::utils::FunctionalFactory::get().build(func, params);
         (*test)(10);
         (*test)({10});
+        CG_LOG << "Test 4 failed";
+        return -1;
       } catch (const cepgen::Exception& e) {
-        passed = true;
+        CG_LOG << "Test 4 passed.";
       }
-      if (!passed)
-        throw CG_FATAL("main") << "Test 4 failed!";
-      CG_LOG << "Test 4 passed!";
     }
   }
 
