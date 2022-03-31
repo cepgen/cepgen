b3d0ac98b8dd2f5c93e0b890819d219815b5f81f
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-03-31 23:31:23 +0200

    Cuba: stripped Cuba implementation from header ; fixed a bug where the integrand was not properly initialised

--- CepGenAddOns/CubaWrapper/IntegratorCuba.h
+++ CepGenAddOns/CubaWrapper/IntegratorCuba.h
@@ -5,29 +5,14 @@
 #include "CepGen/Integration/Integrator.h"
 
 namespace cepgen {
+  class Integrand;
   /// Cuba integration algorithm
   class IntegratorCuba : public Integrator {
   public:
-    explicit IntegratorCuba(const ParametersList& params)
-        : Integrator(params),
-          nvec_(steer<int>("nvec")),
-          epsrel_(steer<double>("epsrel")),
-          epsabs_(steer<double>("epsabs")),
-          mineval_(steer<int>("mineval")),
-          maxeval_(steer<int>("maxeval")),
-          verbose_(steer<int>("verbose")) {}
+    explicit IntegratorCuba(const ParametersList&);
 
-    static ParametersDescription description() {
-      auto desc = Integrator::description();
-      desc.setDescription("Cuba generic integration algorithm");
-      desc.add<int>("nvec", 1).setDescription("number of samples received by the integrand");
-      desc.add<double>("epsrel", 1.e-3).setDescription("requested relative accuracy");
-      desc.add<double>("epsabs", 1.e-12).setDescription("requested absolute accuracy");
-      desc.add<int>("mineval", 0).setDescription("minimum number of integrand evaluations required");
-      desc.add<int>("maxeval", 50000).setDescription("(approximate) maximum number of integrand evaluations allowed");
-      desc.add<int>("verbose", 1);
-      return desc;
-    }
+    static ParametersDescription description();
+    void setIntegrand(Integrand&) override;
 
   protected:
     int nvec_;
@@ -36,11 +21,8 @@ namespace cepgen {
     int verbose_;
   };
 
-  static Integrand* gIntegrand = nullptr;
-  inline int cuba_integrand(const int* ndim, const double xx[], const int* /*ncomp*/, double ff[], void* /*userdata*/) {
-    ff[0] = gIntegrand->eval(std::vector<double>(xx, xx + *ndim));
-    return 0;
-  }
+  static Integrand* gIntegrand;
+  int cuba_integrand(const int* ndim, const double xx[], const int* /*ncomp*/, double ff[], void* /*userdata*/);
 }  // namespace cepgen
 
 #endif
