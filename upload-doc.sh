fd480c4e124afbb39ee876af9de61bd1dac4ab01
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-04-04 17:58:00 +0200

    Sphinx: slightly modified output paths, and matching new versions APIs

--- upload-doc.sh
+++ upload-doc.sh
@@ -1,1 +1,1 @@
-rsync -arvz -e 'ssh -p 222' --delete doc/docs/ lforthomme@login.hepforge.org:~/cepgen/public_html/
+rsync -arvz -e 'ssh -p 222' --delete doc/output_html/ lforthomme@login.hepforge.org:~/cepgen/public_html/
