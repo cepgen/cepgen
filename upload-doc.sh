d3cea833356cde4367a1ff80dec3a355a250a79e
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-04-05 14:06:33 +0200

    Doc: generating the bare version of the raw modules list

--- upload-doc.sh
+++ upload-doc.sh
@@ -1,3 +1,3 @@
-./cepgenDocGenerator -o ../doc/.raw_modules.html -b 0
+./cepgenDocGenerator -o ../doc/.raw_modules.html -e
 make Sphinx
 rsync -arvz -e 'ssh -p 222' --delete doc/output_html/ lforthomme@login.hepforge.org:~/cepgen/public_html/
