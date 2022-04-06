6bd61b69e4cf83836b940eea72197046c1a96dc7
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-04-06 12:20:51 +0200

    Build: moving back to former delphes/root version

--- source-lxplus.sh
+++ source-lxplus.sh
@@ -9,9 +9,8 @@ export CMAKE_DIR=${CVMFS_DIR}/CMake/3.17.3-75516/x86_64-centos7-gcc9-opt
 export PATH=${CMAKE_DIR}/bin:${PATH}
 export Boost_DIR=${CVMFS_DIR}/Boost/1.78.0-e410e/x86_64-centos7-gcc9-opt
 #--- Delphes linking
-source ${CVMFS_DIR}/ROOT/6.26.00-165a8/x86_64-centos7-gcc9-opt/bin/thisroot.sh
-export nlohmann_json_DIR=${CVMFS_DIR}/jsonmcpp/3.9.1-72770/x86_64-centos7-gcc9-opt
-export DELPHES_DIR=${CVMFS_DIR}/delphes/3.5.0-2f649/x86_64-centos7-gcc8-opt
+source ${CVMFS_DIR}/ROOT/v6.24.00-f4a14/x86_64-centos7-gcc9-opt/bin/thisroot.sh
+export DELPHES_DIR=${CVMFS_DIR}/delphes/3.4.3pre09-604d4/x86_64-centos7-gcc9-opt
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:${DELPHES_DIR}/include
 #--- Pythia linking
 export PYTHIA6_DIR=${CVMFS_DIR}/MCGenerators/pythia6/429.2-5d3d1/x86_64-centos7-gcc9-opt
@@ -27,7 +26,7 @@ export LHAPDF_PATH=${CVMFS_DIR}/MCGenerators/lhapdf/6.4.0-0fdec/x86_64-centos7-g
 export TBB_DIR=${CVMFS_DIR}/tbb/2020_U2-daa7e/x86_64-centos7-gcc9-opt
 export VDT_DIR=${CVMFS_DIR}/vdt/0.4.3-992df/x86_64-centos7-gcc9-opt
 export LD_LIBRARY_PATH=${TBB_DIR}/lib:${VDT_DIR}/lib:${LD_LIBRARY_PATH}
-export JSONMCPP_DIR=${CVMFS_DIR}/jsonmcpp/3.9.1-72770/x86_64-centos7-gcc9-opt
+export nlohmann_json_DIR=${CVMFS_DIR}/jsonmcpp/3.9.1-72770/x86_64-centos7-gcc9-opt
 
 echo "Environment prepared for LXPLUS"
 export CEPGEN_LXPLUS_ENV=1
