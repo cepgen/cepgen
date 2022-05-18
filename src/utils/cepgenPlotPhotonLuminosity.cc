5bcd5f4a2b0b1b1c10b145c827a5fb34b828a78a
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-05-18 19:32:28 +0200

    Utils: a few improvements in plottng script
    - fixed collinear fluxes output files
    - symmetric x cut for RPs acceptance in two-photon luminosity computation
    - allow to set a user-defined rescaling and a centre-of-mass energy for each flux
    - allowing multiple xi acceptance definitions when plotting two-photon luminosity
    - better type for number of points
    - showing proper unit if scaling is specified ; set higher nodes multiplicity for integration
    - adapted consumer code to new analytic integrator API

--- src/utils/cepgenPlotPhotonLuminosity.cc
+++ src/utils/cepgenPlotPhotonLuminosity.cc
@@ -21,30 +21,33 @@
 #include "CepGen/CollinearFluxes/Parameterisation.h"
 #include "CepGen/Core/Exception.h"
 #include "CepGen/Generator.h"
+#include "CepGen/Integration/AnalyticIntegrator.h"
+#include "CepGen/Modules/AnalyticIntegratorFactory.h"
 #include "CepGen/Modules/CollinearFluxFactory.h"
 #include "CepGen/Modules/DrawerFactory.h"
 #include "CepGen/Utils/ArgumentsParser.h"
 #include "CepGen/Utils/Drawer.h"
-#include "CepGen/Utils/GSLIntegrator.h"
 #include "CepGen/Utils/Graph.h"
 #include "CepGen/Utils/String.h"
 
 using namespace std;
-using namespace cepgen;
 
 int main(int argc, char* argv[]) {
   vector<int> cfluxes;
   int num_points;
-  double q2max, sqrts, mxmin, mxmax;
-  string output_file, plotter;
+  double q2max, mxmin, mxmax;
+  string integrator, output_file, plotter;
   bool logx, logy, draw_grid;
-  vector<double> xi_range_vec;
-  Limits y_range, xi_range;
+  vector<double> rescl, sqrts, accept;
+  cepgen::Limits y_range;
+  vector<cepgen::Limits> xi_ranges(1);
 
   cepgen::ArgumentsParser(argc, argv)
-      .addOptionalArgument("collflux,i", "collinear flux modelling(s)", &cfluxes, vector<int>{1})
+      .addOptionalArgument("collflux,f", "collinear flux modelling(s)", &cfluxes, vector<int>{1})
+      .addOptionalArgument("rescaling,r", "luminosity rescaling", &rescl, vector<double>{1.})
+      .addOptionalArgument("integrator,i", "type of integration algorithm", &integrator, "gsl")
       .addOptionalArgument("q2max,q", "maximum Q^2", &q2max, 1000.)
-      .addOptionalArgument("sqrts,s", "two-proton centre of mass energy (GeV)", &sqrts, 13.e3)
+      .addOptionalArgument("sqrts,s", "two-proton centre of mass energy (GeV)", &sqrts, vector<double>{13.e3})
       .addOptionalArgument("mxmin,m", "minimal two-photon mass", &mxmin, 0.)
       .addOptionalArgument("mxmax,M", "maximal two-photon mass", &mxmax, 100.)
       .addOptionalArgument("npoints,n", "number of x-points to scan", &num_points, 500)
@@ -52,8 +55,9 @@ int main(int argc, char* argv[]) {
       .addOptionalArgument("plotter,p", "type of plotter to user", &plotter, "")
       .addOptionalArgument("logx", "logarithmic x-scale", &logx, false)
       .addOptionalArgument("logy,l", "logarithmic y-scale", &logy, false)
-      .addOptionalArgument("xi-range,x", "acceptance range for proton momentum loss", &xi_range, Limits{})
-      .addOptionalArgument("y-range,y", "y plot range", &y_range, Limits{})
+      .addOptionalArgument("xi-range,x", "acceptance range for proton momentum loss", &xi_ranges[0])
+      .addOptionalArgument("accept", "pairs of min/max acceptance ranges for proton momentum loss", &accept)
+      .addOptionalArgument("yrange,y", "y plot range", &y_range)
       .addOptionalArgument("draw-grid,g", "draw the x/y grid", &draw_grid, false)
       .parse();
 
@@ -62,37 +66,70 @@ int main(int argc, char* argv[]) {
   ofstream out(output_file);
   if (logx && mxmin == 0.)
     mxmin = 1.e-3;
+  if (sqrts.size() != cfluxes.size())
+    sqrts = vector<double>(cfluxes.size(), sqrts.at(0));
+  if (rescl.size() != cfluxes.size())
+    rescl = vector<double>(cfluxes.size(), rescl.at(0));
+  if (!accept.empty()) {
+    xi_ranges.clear();
+    if (accept.size() % 2 != 0)
+      throw CG_FATAL("main")
+          << "Invalid acceptance(s) list specified! Supported format is an array of (min1, max2, min2, max2, ...)";
+    for (size_t i = 0; i < accept.size() / 2; ++i) {
+      cepgen::Limits rng{accept.at(2 * i), accept.at(2 * i + 1)};
+      if (rng == cepgen::Limits{0., 1.})
+        rng = cepgen::Limits();
+      xi_ranges.emplace_back(rng);
+    }
+    if (!xi_ranges.empty())
+      CG_LOG << "x (xi) acceptance cuts defined: " << xi_ranges << ".";
+  }
 
   out << "# coll. fluxes: " << cepgen::utils::merge(cfluxes, ",") << "\n"
-      << "# two-photon mass range: " << cepgen::Limits(mxmin, mxmax) << "\n";
-  map<int, cepgen::utils::Graph1D> m_gr_fluxes;  // {collinear flux -> graph}
-  auto integr = cepgen::utils::GSLIntegrator();
-  const auto s = sqrts * sqrts;
-  for (const auto& cflux : cfluxes) {
+      << "# two-photon mass range: " << cepgen::Limits(mxmin, mxmax);
+  map<int, vector<cepgen::utils::Graph1D> > m_gr_fluxes;  // {collinear flux -> graph}
+  vector<double> mxvals;
+  for (int j = 0; j < num_points; ++j)
+    mxvals.emplace_back((!logx) ? mxmin + j * (mxmax - mxmin) / (num_points - 1)
+                                : pow(10, log10(mxmin) + j * (log10(mxmax) - log10(mxmin)) / (num_points - 1)));
+  vector<vector<double> > values(num_points);
+  auto integr = cepgen::AnalyticIntegratorFactory::get().build(
+      cepgen::ParametersList().setName<string>(integrator).set<int>("mode", 0).set<int>("nodes", 2000));
+  for (size_t i = 0; i < cfluxes.size(); ++i) {
+    const auto& cflux = cfluxes.at(i);
+    const auto s = sqrts.at(i) * sqrts.at(i);
     auto coll_flux = cepgen::collflux::CollinearFluxFactory::get().build(cflux);
-    ostringstream oss;
-    oss << cflux;
-    m_gr_fluxes[cflux].setTitle(oss.str());
+    for (const auto& xi_range : xi_ranges) {
+      ostringstream oss;
+      oss << (cepgen::collflux::Type)cflux;
+      if (xi_range.valid())
+        oss << " (" << xi_range.min() << " < \\xi < " << xi_range.max() << ")";
+      m_gr_fluxes[cflux].emplace_back();
+      m_gr_fluxes[cflux].back().setTitle(oss.str());
+    }
 
     for (int j = 0; j < num_points; ++j) {
-      const double mx = (!logx) ? mxmin + j * (mxmax - mxmin) / (num_points - 1)
-                                : pow(10, log10(mxmin) + j * (log10(mxmax) - log10(mxmin)) / (num_points - 1));
-      auto lumi_wgg = integr.eval(
-          [&xi_range, &mx, &s, &coll_flux](double x) {
-            if (xi_range.valid() && !xi_range.contains(x))
-              return 0.;
-            return 2. * mx / x / s * (*coll_flux)(x) * (*coll_flux)(mx * mx / x / s);
-          },
-          mx * mx / s,
-          1.);
-      out << mx << "\t" << lumi_wgg << "\n";
-      m_gr_fluxes[cflux].addPoint(mx, lumi_wgg);
+      const auto& mx = mxvals.at(j);
+      for (size_t k = 0; k < xi_ranges.size(); ++k) {
+        const auto& xi_range = xi_ranges.at(k);
+        auto lumi_wgg = integr->eval(
+            [&xi_range, &mx, &s, &coll_flux](double x) {
+              if (xi_range.valid() && (!xi_range.contains(x) || !xi_range.contains(mx * mx / x / s)))
+                return 0.;
+              return 2. * mx / x / s * (*coll_flux)(x) * (*coll_flux)(mx * mx / x / s);
+            },
+            cepgen::Limits(mx * mx / s, 1.));
+        lumi_wgg *= rescl.at(i);
+        values.at(j).emplace_back(lumi_wgg);
+        m_gr_fluxes[cflux][k].addPoint(mx, lumi_wgg);
+      }
     }
   }
+  for (int i = 0; i < num_points; ++i)
+    out << "\n" << mxvals.at(i) << "\t" << cepgen::utils::merge(values.at(i), "\t");
   out.close();
 
   if (!plotter.empty()) {
-    ostringstream oss;
     auto plt = cepgen::utils::DrawerFactory::get().build(plotter);
     cepgen::utils::Drawer::Mode dm;
     if (logx)
@@ -102,17 +139,23 @@ int main(int argc, char* argv[]) {
     if (draw_grid)
       dm |= cepgen::utils::Drawer::Mode::grid;
     cepgen::utils::DrawableColl coll;
+    size_t i = 0;
     for (auto& cf_gr : m_gr_fluxes) {
-      cf_gr.second.xAxis().setLabel("$\\omega_{\\gamma\\gamma}$ (GeV$^{-1}$)");
-      cf_gr.second.yAxis().setLabel("d$L_{\\gamma\\gamma}$/d$\\omega_{\\gamma\\gamma}$ (GeV$^{-1}$)");
-      if (y_range.valid())
-        cf_gr.second.yAxis().setRange(y_range);
-      cf_gr.second.setTitle(
-          cepgen::utils::format("%s", cepgen::collflux::CollinearFluxFactory::get().describe(cf_gr.first).data()));
-      coll.emplace_back(&cf_gr.second);
+      for (auto& gr_xi : cf_gr.second) {
+        string units;
+        if (rescl.at(i) != 1.)
+          units = "cm$^{-2}$ s$^{-1}$ ";
+        gr_xi.xAxis().setLabel("$\\omega_{\\gamma\\gamma}$ (GeV)");
+        gr_xi.yAxis().setLabel("d$L_{\\gamma\\gamma}$/d$\\omega_{\\gamma\\gamma}$ (" + units + "GeV$^{-1}$)");
+        if (y_range.valid())
+          gr_xi.yAxis().setRange(y_range);
+        //gr_xi.setTitle(
+        //    cepgen::utils::format("%s", cepgen::collflux::CollinearFluxFactory::get().describe(cf_gr.first).data()));
+        coll.emplace_back(&gr_xi);
+      }
+      ++i;
     }
-    plt->draw(coll, "comp_photonlumi", oss.str(), dm);
+    plt->draw(coll, "comp_photonlumi", "", dm);
   }
-
   return 0;
 }
