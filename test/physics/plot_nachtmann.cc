3c7d318d4526a61621accebfe531b2e57c17e385
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-06-22 13:56:07 +0200

    Tests: added missing complex include file for ubuntu bionic?

--- test/physics/plot_nachtmann.cc
+++ test/physics/plot_nachtmann.cc
@@ -16,6 +16,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <ccomplex>
+
 #include "CepGen/Core/Exception.h"
 #include "CepGen/Core/ParametersList.h"
 #include "CepGen/Generator.h"
