8807736d3a2385b2b7f0a91a13af28160dc4ea41
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-08-22 14:49:03 +0200

    Utils: renamed GSL analytic integrator

--- CepGen/Utils/AnalyticalIntegratorGSL.cpp
+++ CepGen/Utils/AnalyticalIntegratorGSL.cpp
@@ -29,11 +29,32 @@
 
 namespace cepgen {
   namespace utils {
-    class GSLIntegrator final : public AnalyticIntegrator {
+    class AnalyticalIntegratorGSL final : public AnalyticIntegrator {
     public:
-      explicit GSLIntegrator(const ParametersList&);
+      explicit AnalyticalIntegratorGSL(const ParametersList& params)
+          : AnalyticIntegrator(params),
+            mode_(steerAs<int, Mode>("mode")),
+            fixed_type_(steerAs<int, FixedType>("fixedType")),
+            nodes_(steer<int>("nodes")),
+            alpha_(steer<double>("alpha")),
+            beta_(steer<double>("beta")),
+            limit_(steerAs<int, size_t>("limit")),
+            epsabs_(steer<double>("epsabs")),
+            epsrel_(steer<double>("epsrel")) {}
 
-      static ParametersDescription description();
+      static ParametersDescription description() {
+        auto desc = AnalyticIntegrator::description();
+        desc.setDescription("GSL 1D integration algorithms wrapper");
+        desc.addAs<int, Mode>("mode", Mode::Fixed).setDescription("integrator algorithm to use");
+        desc.addAs<int, FixedType>("fixedType", FixedType::Jacobi).setDescription("type of quadrature");
+        desc.add<int>("nodes", 100).setDescription("number of quadrature nodes for the fixed type integration");
+        desc.add<double>("alpha", 0.).setDescription("alpha parameter for the fixed type integration");
+        desc.add<double>("beta", 0.).setDescription("alpha parameter for the fixed type integration");
+        desc.add<int>("limit", 1000).setDescription("maximum number of subintervals to build");
+        desc.add<double>("epsabs", 0.).setDescription("desired absolute error limit");
+        desc.add<double>("epsrel", 0.1).setDescription("desired relative error limit");
+        return desc;
+      }
 
       double eval(const Function1D& func, void* obj = nullptr, const Limits& lim = {}) const override {
         if (obj)
@@ -64,32 +85,7 @@ namespace cepgen {
       static constexpr double INVALID = -999.999;
     };
 
-    GSLIntegrator::GSLIntegrator(const ParametersList& params)
-        : AnalyticIntegrator(params),
-          mode_(steerAs<int, Mode>("mode")),
-          fixed_type_(steerAs<int, FixedType>("fixedType")),
-          nodes_(steer<int>("nodes")),
-          alpha_(steer<double>("alpha")),
-          beta_(steer<double>("beta")),
-          limit_(steerAs<int, size_t>("limit")),
-          epsabs_(steer<double>("epsabs")),
-          epsrel_(steer<double>("epsrel")) {}
-
-    ParametersDescription GSLIntegrator::description() {
-      auto desc = AnalyticIntegrator::description();
-      desc.setDescription("GSL 1D integration algorithms wrapper");
-      desc.addAs<int, Mode>("mode", Mode::Fixed).setDescription("integrator algorithm to use");
-      desc.addAs<int, FixedType>("fixedType", FixedType::Jacobi).setDescription("type of quadrature");
-      desc.add<int>("nodes", 100).setDescription("number of quadrature nodes for the fixed type integration");
-      desc.add<double>("alpha", 0.).setDescription("alpha parameter for the fixed type integration");
-      desc.add<double>("beta", 0.).setDescription("alpha parameter for the fixed type integration");
-      desc.add<int>("limit", 1000).setDescription("maximum number of subintervals to build");
-      desc.add<double>("epsabs", 0.).setDescription("desired absolute error limit");
-      desc.add<double>("epsrel", 0.1).setDescription("desired relative error limit");
-      return desc;
-    }
-
-    double GSLIntegrator::eval(const gsl_function* wrp, const Limits& lim) const {
+    double AnalyticalIntegratorGSL::eval(const gsl_function* wrp, const Limits& lim) const {
       const double xmin = (lim.hasMin() ? lim.min() : range_.min());
       const double xmax = (lim.hasMax() ? lim.max() : range_.max());
       double result{0.};
@@ -125,7 +121,7 @@ namespace cepgen {
             type = gsl_integration_fixed_chebyshev2;
             break;
           default:
-            throw CG_FATAL("GSLIntegrator") << "Invalid fixed quadrature type: " << (int)fixed_type_ << ".";
+            throw CG_FATAL("AnalyticalIntegratorGSL") << "Invalid fixed quadrature type: " << (int)fixed_type_ << ".";
         }
         std::unique_ptr<gsl_integration_fixed_workspace, void (*)(gsl_integration_fixed_workspace*)> workspace(
             gsl_integration_fixed_alloc(type, nodes_, xmin, xmax, alpha_, beta_), gsl_integration_fixed_free);
@@ -148,10 +144,11 @@ namespace cepgen {
               const_cast<gsl_function*>(wrp), xmin, xmax, epsabs_, epsrel_, 0., limit_, workspace.get(), &result, &error);
       }
       if (res != GSL_SUCCESS)
-        CG_WARNING("GSLIntegrator") << "Failed to evaluate the integral. GSL error: " << gsl_strerror(res) << ".";
+        CG_WARNING("AnalyticalIntegratorGSL")
+            << "Failed to evaluate the integral. GSL error: " << gsl_strerror(res) << ".";
       return result;
     }
   }  // namespace utils
 }  // namespace cepgen
 
-REGISTER_ANALYTIC_INTEGRATOR("gsl", GSLIntegrator, utils::GSLIntegrator)
+REGISTER_ANALYTIC_INTEGRATOR("gsl", AnalyticalIntegratorGSL, utils::AnalyticalIntegratorGSL)
