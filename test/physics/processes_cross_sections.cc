a6538752e5435ac5b99dcdb2cdb96c15db896e35
Laurent Forthomme <laurent.forthomme@cern.ch>
2022-12-12 14:26:53 +0100

    Test: simplified processes cross section computation test

--- test/physics/processes_cross_sections.cc
+++ test/physics/processes_cross_sections.cc
@@ -25,7 +25,7 @@
 #include "CepGen/Utils/AbortHandler.h"
 #include "CepGen/Utils/ArgumentsParser.h"
 #include "CepGen/Utils/ProgressBar.h"
-#include "CepGen/Utils/String.h"
+#include "CepGen/Utils/Test.h"
 #include "CepGen/Utils/Timer.h"
 
 using namespace std;
@@ -34,24 +34,22 @@ int main(int argc, char* argv[]) {
   double num_sigma;
   string cfg_filename;
   string integrator;
-  bool quiet;
+  bool verbose;
 
-  cepgen::ArgumentsParser argparse(argc, argv);
-  argparse.addArgument("cfg,f", "configuration file", &cfg_filename)
-      .addOptionalArgument("quiet,q", "quiet mode", &quiet, false)
+  cepgen::ArgumentsParser(argc, argv)
+      .addOptionalArgument("cfg,f", "configuration file", &cfg_filename, "test/physics/test_processes.cfg")
+      .addOptionalArgument("verbose,v", "verbose mode", &verbose, false)
       .addOptionalArgument("num-sigma,n", "max. number of std.dev.", &num_sigma, 3.)
       .addOptionalArgument("integrator,i", "type of integrator used", &integrator, "Vegas")
       .parse();
 
-  if (quiet)
-    cepgen::utils::Logger::get().level = cepgen::utils::Logger::Level::error;
+  if (!verbose)
+    cepgen::utils::Logger::get().level = cepgen::utils::Logger::Level::warning;
 
   cepgen::utils::Timer tmr;
   cepgen::Generator gen;
 
   CG_LOG << "Testing with " << integrator << " integrator.";
-
-  vector<string> failed_tests, passed_tests;
 
   CG_LOG << "Initial configuration time: " << tmr.elapsed() * 1.e3 << " ms.";
   tmr.reset();
@@ -83,23 +81,16 @@ int main(int argc, char* argv[]) {
 
   CG_LOG << "Will run " << cepgen::utils::s("test", tests.size()) << ".";
 
-  std::unique_ptr<cepgen::utils::ProgressBar> progress;
-  if (argparse.debugging())
-    progress.reset(new cepgen::utils::ProgressBar(tests.size()));
-
-  unsigned short num_tests = 0;
   for (const auto& test : tests) {
     const std::string filename = "test/physics/test_processes/" + test.filename + "_cfg.py";
     try {
       gen.parametersRef().clearProcess();
       gen.setParameters(cepgen::card::Handler::parse(filename));
 
-      CG_DEBUG("main") << gen.parameters();
-
       gen.parametersRef().par_integrator.setName<std::string>(integrator);
-      CG_LOG << "Process: " << gen.parameters()->processName() << "\n\t"
-             << "File: " << filename << "\n\t"
-             << "Configuration time: " << tmr.elapsed() * 1.e3 << " ms.";
+      CG_DEBUG("main") << "Process: " << gen.parameters()->processName() << "\n\t"
+                       << "File: " << filename << "\n\t"
+                       << "Configuration time: " << tmr.elapsed() * 1.e3 << " ms.";
 
       tmr.reset();
 
@@ -110,45 +101,23 @@ int main(int argc, char* argv[]) {
       const double err_ratio = ratio * hypot(err_new_cs / new_cs, test.err_ref_cs / test.ref_cs);
       const double pull = (new_cs - test.ref_cs) / hypot(err_new_cs, test.err_ref_cs);
 
-      const bool success = fabs(pull) < num_sigma;
-
-      CG_LOG << "Computed cross section:\n\t"
-             << "Ref.   = " << test.ref_cs << " +/- " << test.err_ref_cs << "\n\t"
-             << "CepGen = " << new_cs << " +/- " << err_new_cs << "\n\t"
-             << "Ratio: " << ratio << " +/- " << err_ratio << "\n\t"
-             << "Pull: " << pull << " (abs(pull) " << (success ? "<" : ">") << " " << num_sigma << ").\n\t"
-             << "Computation time: " << tmr.elapsed() * 1.e3 << " ms.";
+      CG_DEBUG("main") << "Computed cross section:\n\t"
+                       << "Ref.   = " << test.ref_cs << " +/- " << test.err_ref_cs << "\n\t"
+                       << "CepGen = " << new_cs << " +/- " << err_new_cs << "\n\t"
+                       << "Ratio: " << ratio << " +/- " << err_ratio << "\n\t"
+                       << "Pull: " << pull << ".\n\t"
+                       << "Computation time: " << tmr.elapsed() * 1.e3 << " ms.";
       tmr.reset();
 
       const string test_res = cepgen::utils::format(
           "%-40s\tref=%g\tgot=%g\tratio=%g\tpull=%+10.5f", test.filename.c_str(), test.ref_cs, new_cs, ratio, pull);
-      if (success)
-        passed_tests.emplace_back(test_res);
-      else
-        failed_tests.emplace_back(test_res);
-      ++num_tests;
-      if (argparse.debugging())
-        progress->update(num_tests);
-      CG_LOG << "Test " << num_tests << "/" << tests.size() << " finished. "
-             << "Success: " << cepgen::utils::yesno(success) << ".";
+      CG_TEST(fabs(pull) < num_sigma, filename);
+    } catch (const cepgen::utils::RunAbortedException&) {
+      CG_TEST_SUMMARY;
     } catch (const cepgen::Exception& e) {
       CG_LOG << "Test \"" << test.filename << "\" (located at " << filename << ") failed.";
       cepgen::Exception(e).dump();
     }
   }
-  if (failed_tests.size() != 0) {
-    ostringstream os_failed, os_passed;
-    for (const auto& fail : failed_tests)
-      os_failed << "  " << fail << endl;
-    for (const auto& pass : passed_tests)
-      os_passed << "  " << pass << endl;
-    throw CG_FATAL("main") << "Some tests failed (abs(pull) > " << num_sigma << "):\n"
-                           << os_failed.str() << "\n "
-                           << "Passed tests:\n"
-                           << os_passed.str() << ".";
-  }
-
-  CG_LOG << "ALL TESTS PASSED!";
-
-  return 0;
+  CG_TEST_SUMMARY;
 }
