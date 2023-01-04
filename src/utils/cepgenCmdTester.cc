8bb5f86adaad97ef2d93487bdec7cc67104f6230
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-01-04 13:54:34 +0100

    Utils: added a flag to reserialise the parsed arguments

--- src/utils/cepgenCmdTester.cc
+++ src/utils/cepgenCmdTester.cc
@@ -19,19 +19,29 @@
 #include "CepGen/Core/ParametersDescription.h"
 #include "CepGen/Utils/ArgumentsParser.h"
 #include "CepGen/Utils/Message.h"
+#include "CepGen/Utils/String.h"
 #include "CepGen/Version.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
   string commands;
+  bool reserialise;
   cepgen::ArgumentsParser(argc, argv)
       .addOptionalArgument("cmd,c", "list of arguments to be parsed", &commands, "")
+      .addOptionalArgument("serial,s", "reserialise the parsed arguments", &reserialise, false)
       .parse();
 
   auto plist = cepgen::ParametersList().feed(commands);
   CG_LOG << "CepGen v" << cepgen::version::tag << " command line arguments parser parsed the following arguments:\n"
          << cepgen::ParametersDescription(plist);
+
+  if (reserialise) {
+    const auto serialised = plist.serialise();
+    const auto col = serialised == commands ? cepgen::utils::Colour::green : cepgen::utils::Colour::yellow;
+    CG_LOG << "Reserialised arguments: " << cepgen::utils::colourise(serialised, col, cepgen::utils::Modifier::italic)
+           << ".";
+  }
 
   return 0;
 }
