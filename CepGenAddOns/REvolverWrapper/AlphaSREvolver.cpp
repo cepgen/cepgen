965b01b474acdcbc7439a788b7d025a625991a8d
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-02-08 14:54:42 +0100

    REvolver: added a first implementation of the alphaS evolution algo

---
+++ CepGenAddOns/REvolverWrapper/AlphaSREvolver.cpp
@@ -0,0 +1,52 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2013-2021  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <REvolver.h>
+
+#include "CepGen/Modules/CouplingFactory.h"
+#include "CepGen/Physics/Coupling.h"
+
+namespace cepgen {
+  class AlphaSREvolver final : public Coupling {
+  public:
+    explicit AlphaSREvolver(const ParametersList& params)
+        : Coupling(params),
+          qc_(steer<double>("qCentral")),
+          qevol_(steer<double>("qEvol")),
+          central2_(revo::RunPar(5, qc_, qevol_), 2) {
+      const double q_min = qc_ - 0.0031, q_max = qc_ + 0.0034;
+      revo::Core min2(revo::RunPar(5, q_min, qevol_), 2);
+      revo::Core max2(revo::RunPar(5, q_max, qevol_), 2);
+    }
+    static ParametersDescription description() {
+      auto desc = Coupling::description();
+      desc.setDescription("REvolver alpha(S) evolution algorithm");
+      desc.add<double>("qCentral", 0.0822);
+      desc.add<double>("qEvol", 1508.04);
+      return desc;
+    }
+
+    double operator()(double q) const override { return central2_.alpha(q); }
+
+  private:
+    const double qc_, qevol_;
+    revo::Core central2_;
+  };
+}  // namespace cepgen
+
+REGISTER_ALPHAS_MODULE("revolver", AlphaSREvolver)
