5865ea6a7a74cd7e1d0145aa1a9d60a56390c797
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-05-09 11:37:46 +0200

    Test+Python: improved python test debugging

--- CepGenAddOns/PythonWrapper/test/python_particle_mod.cc
+++ CepGenAddOns/PythonWrapper/test/python_particle_mod.cc
@@ -10,10 +10,13 @@ int main(int argc, char* argv[]) {
   cepgen::ArgumentsParser(argc, argv).parse();
 
   auto card = cepgen::CardsHandlerFactory::get().build(".py");
+  CG_DEBUG("main") << "Python cards handler successfully constructed.";
+
   card->parseString(R"(
 from Config.PDG_cfi import PDG, registerParticle
 registerParticle(name='teston', pdgid=42, mass=42.42, width=1.1))",
                     new cepgen::Parameters);
+  CG_DEBUG("main") << "Configuration string successfully parsed.";
 
   const auto& teston = cepgen::PDG::get()(42);
   CG_TEST_EQUAL(teston.pdgid, 42, "new particle PDG id");
