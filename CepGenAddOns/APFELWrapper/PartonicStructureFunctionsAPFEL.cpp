1bb19398331698f5b01a4519a87271813d3c887f
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-06-05 18:16:27 +0200

    LHAPDF+APFEL: simplified partonic SF computation part, added APFEL PDF evaluation

---
+++ CepGenAddOns/APFELWrapper/PartonicStructureFunctionsAPFEL.cpp
@@ -0,0 +1,64 @@
+/*
+ *  CepGen: a central exclusive processes event generator
+ *  Copyright (C) 2023  Laurent Forthomme
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <APFEL/APFEL.h>
+
+#include <cmath>
+
+#include "CepGen/Core/Exception.h"
+#include "CepGen/Modules/StructureFunctionsFactory.h"
+#include "CepGen/StructureFunctions/PartonicParameterisation.h"
+
+namespace cepgen {
+  namespace strfun {
+    /// Generic partonic level perturbative structure functions built from an external PDFs grid
+    class APFELPartonic final : public PartonicParameterisation {
+    public:
+      /// Quarks types
+      enum class Mode { full = 0, valence = 1, sea = 2 };
+      /// Build a calculator from its Parameters object
+      explicit APFELPartonic(const ParametersList& params) : PartonicParameterisation(params) {
+        const auto perturbative_order = steer<int>("perturbativeOrder");
+        APFEL::SetPerturbativeOrder(perturbative_order);
+        APFEL::InitializeAPFEL();
+        const auto q_limits = steer<Limits>("qLimits");
+        APFEL::EvolveAPFEL(q_limits.min(), q_limits.max());
+        APFEL::CachePDFsAPFEL(q_limits.min());
+        CG_INFO("APFELPartonic") << "Partonic structure functions evaluator successfully built.\n"
+                                 << " * APFEL version: " << APFEL::GetVersion() << "\n"
+                                 << " * number of flavours: " << num_flavours_ << "\n"
+                                 << " * quarks mode: " << mode_ << "\n"
+                                 << " * perturbative order: " << perturbative_order << ".";
+      }
+
+      static ParametersDescription description() {
+        auto desc = PartonicParameterisation::description();
+        desc.setDescription("APFEL (partonic)");
+        desc.add<int>("perturbativeOrder", 2);
+        desc.add<Limits>("qLimits", {1., 100.});
+        return desc;
+      }
+
+    private:
+      double evalxQ2(int flavour, double xbj, double q2) override { return APFEL::xPDFxQ(flavour, xbj, std::sqrt(q2)); }
+    };
+  }  // namespace strfun
+}  // namespace cepgen
+
+using cepgen::strfun::APFELPartonic;
+REGISTER_STRFUN(402, APFELPartonic);
