0d1961ca869acf71fb14d8116d8724de7037038d
Laurent Forthomme <laurent.forthomme@cern.ch>
2023-12-07 21:06:20 +0100

    Python: added extra structure functions modellings to includes

--- python/Config/StructureFunctions_cfi.py
+++ python/Config/StructureFunctions_cfi.py
@@ -21,9 +21,13 @@ class StructureFunctions:
     SuriYennie          = generic.clone(11)
     SzczurekUleshchenko = generic.clone(12)
     BlockDurandHa       = generic.clone(13)
+    SuriYennieAlt       = generic.clone(14)
     FioreBrasse         = generic.clone(101)
     ChristyBosted       = generic.clone(102)
     CLAS                = generic.clone(103)
+    FioreBrasseLowMass  = generic.clone(104)
+    DonnachieLandshoff  = generic.clone(105)
+    Capella             = generic.clone(106)
     ALLM91              = generic.clone(201)
     ALLM97              = generic.clone(202)
     GD07p               = generic.clone(203)
@@ -31,15 +35,20 @@ class StructureFunctions:
     MSTWgrid = generic.clone(205,
         gridPath = 'External/F2_Luxlike_fit/mstw_f2_scan_nnlo.dat',
     )
+    HHT                 = generic.clone(206)
+    HHT_FT              = generic.clone(207)
     LUXlike = generic.clone(301,
         #Q2cut = 10.,
         #W2limits = (4.,1.),
         #continuumSF = GD11p,
         #resonancesSF = ChristyBosted,
     )
-    KulaginBarinov = generic.clone(303)
+    Shamov              = generic.clone(302)
+    KulaginBarinov      = generic.clone(303)
+    BodekKangXu         = generic.clone(304)
     Partonic = generic.clone(401,
         pdfSet = 'LUXqed17_plus_PDF4LHC15_nnlo_100',
         numFlavours = 4,
         mode = PDFMode.AllQuarks,
     )
+    APFELPartonic       = generic.clone(402)
