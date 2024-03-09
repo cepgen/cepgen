d675f4256b47dfe292095afac49f341b33094d04
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-03-09 12:45:13 +0100

    Moved template to a template sub-dir

--- CepGenAddOns/MadGraphWrapper/template/MadGraphProcessImpl.cpp
+++ CepGenAddOns/MadGraphWrapper/template/MadGraphProcessImpl.cpp
@@ -1,6 +1,6 @@
 /*
  *  CepGen: a central exclusive processes event generator
- *  Copyright (C) 2020-2023  Laurent Forthomme
+ *  Copyright (C) 2020-2024  Laurent Forthomme
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
