d64cec9d2f6a780a1081d6bff6f10b63c28e24ae
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-03-21 11:22:16 +0100

    Boost: also handle unsigned long long and booleans

--- CepGenAddOns/BoostWrapper/BoostTreeUtils.cpp
+++ CepGenAddOns/BoostWrapper/BoostTreeUtils.cpp
@@ -37,8 +37,12 @@ namespace boost {
       for (const auto& key : params.keys()) {
         if (params.has<::cepgen::ParametersList>(key))
           out.add_child(key, pack(params.get<::cepgen::ParametersList>(key)));
+        else if (params.has<bool>(key))
+          out.put(key, params.get<bool>(key));
         else if (params.has<int>(key))
           out.put(key, params.get<int>(key));
+        else if (params.has<unsigned long long>(key))
+          out.put(key, params.get<unsigned long long>(key));
         else if (params.has<double>(key)) {
           std::ostringstream os;  // ensure floating point storage
           os << std::scientific << params.get<double>(key);
@@ -56,7 +60,8 @@ namespace boost {
         else if (params.has<std::vector<std::string>>(key))
           out.add_child(key, pack(params.get<std::vector<std::string>>(key)));
         else
-          throw ::cepgen::Exception(__FUNC__, "PythonConfigWriter", ::cepgen::Exception::Type::fatal, __FILE__, __LINE__)
+          throw ::cepgen::Exception(
+              __FUNC__, "boost::cepgen::pack", ::cepgen::Exception::Type::fatal, __FILE__, __LINE__)
               << "Failed to recast the key \"" << key << "\" "
               << "with value \"" << params.getString(key) << "\"!";
       }
@@ -120,17 +125,17 @@ namespace boost {
       if (tree.empty())
         return out;
       for (const auto& it : tree) {
-        try {
-          if (it.first.empty())  // this might be a vector
-            try {
+        try {  // this might be a vector
+          if (it.first.empty())
+            try {  // try to parse as a parameters list
               out.operator[]<std::vector<::cepgen::ParametersList>>(DAUGH_KEY).emplace_back(unpack(it.second));
             } catch (const boost::exception&) {
-              try {
+              try {  // try to parse as a float
                 out.operator[]<std::vector<double>>(DAUGH_KEY).emplace_back(it.second.get_value<double>());
               } catch (const boost::exception&) {
-                try {
+                try {  // try to parse as an integer
                   out.operator[]<std::vector<int>>(DAUGH_KEY).emplace_back(it.second.get_value<int>());
-                } catch (const boost::exception&) {
+                } catch (const boost::exception&) {  // does not work, must be a string
                   out.operator[]<std::vector<std::string>>(DAUGH_KEY).emplace_back(it.second.get_value<std::string>());
                 }
               }
@@ -139,15 +144,15 @@ namespace boost {
             add(out, it.first, it.second);
         } catch (const ::cepgen::Exception&) {
           if (it.second.get_value<std::string>().find('.') != std::string::npos)
-            try {
+            try {  // try to parse float if contains a '.'
               out.set<double>(it.first, it.second.get_value<double>());
-            } catch (const boost::exception&) {
+            } catch (const boost::exception&) {  // does not work as a float, must be a string
               out.set<std::string>(it.first, it.second.get_value<std::string>());
             }
           else
-            try {
+            try {  //  try to parse integer
               out.set<int>(it.first, it.second.get_value<int>());
-            } catch (const boost::exception&) {
+            } catch (const boost::exception&) {  // does not work as an integer, must be a string
               out.set<std::string>(it.first, it.second.get_value<std::string>());
             }
         }
@@ -159,6 +164,7 @@ namespace boost {
       auto plist = unpack(tree);
       //--- first check if we have a limits set
       if (plist.keys().size() <= 2 && (plist.has<double>(MIN_KEY) || plist.has<double>(MAX_KEY))) {
+        // we might have a (min, max) limit
         ::cepgen::Limits lim;
         plist.fill<double>(MIN_KEY, lim.min());
         plist.fill<double>(MAX_KEY, lim.max());
