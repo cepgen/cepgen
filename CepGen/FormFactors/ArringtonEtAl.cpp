04a1d9bf4dda7b8232ee7f60528d82d4e509aa88
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-04-03 20:14:50 +0200

    FormFactors: refactoring of Arrington & Mergell&al. form factors

--- CepGen/FormFactors/ArringtonEtAl.cpp
+++ CepGen/FormFactors/ArringtonEtAl.cpp
@@ -1,6 +1,6 @@
 /*
  *  CepGen: a central exclusive processes event generator
- *  Copyright (C) 2013-2021  Laurent Forthomme
+ *  Copyright (C) 2019-2024  Laurent Forthomme
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -26,76 +26,72 @@ namespace cepgen {
     /// \cite Arrington:2007ux
     class ArringtonEtAl final : public Parameterisation {
     public:
-      explicit ArringtonEtAl(const ParametersList&);
+      explicit ArringtonEtAl(const ParametersList& params) : Parameterisation(params), mode_(steer<int>("mode")) {
+        switch (mode_) {
+          case 0:  // original
+            a_e_ = {3.439, -1.602, 0.068};
+            b_e_ = {15.055, 48.061, 99.304, 0.012, 8.650};
+            a_m_ = {-1.465, 1.260, 0.262};
+            b_m_ = {9.627, 0., 0., 11.179, 13.245};
+            break;
+          case 1:  // fit of quoted Ge+dGe values
+            a_e_ = {4.309, -1.108, -0.324};
+            b_e_ = {15.340, 58.321, 124.11, 3.927, 0.589};
+            a_m_ = {-1.472, 1.210, 0.334};
+            b_m_ = {9.486, 0., 0., 9.440, 15.416};
+            break;
+          case 2:  // fit of quoted Ge-dGe values
+            a_e_ = {4.286, -1.281, -0.486};
+            b_e_ = {16.308, 54.535, 138.03, 7.005, 0.014};
+            a_m_ = {-1.374, 1.080, 0.124};
+            b_m_ = {10.003, 0., 0., 7.680, 9.009};
+            break;
+          case 3:  // fit of quoted Ge values
+            a_e_ = {4.109, -1.052, -0.375};
+            b_e_ = {15.602, 55.519, 123.96, 11.403, 1.931};
+            a_m_ = {-1.436, 1.196, 0.210};
+            b_m_ = {9.721, 0., 0., 9.623, 11.817};
+            break;
+        }
+      }
 
-      static ParametersDescription description();
+      inline static ParametersDescription description() {
+        auto desc = Parameterisation::description();
+        desc.setDescription("Arrington et al.");
+        desc.add<int>("mode", 0)
+            .setDescription("Parameterisation mode")
+            .allow(0, "original")
+            .allow(1, "fit of quoted Ge+dGe values")
+            .allow(2, "fit of quoted Ge-dGe values")
+            .allow(3, "fit of quoted Ge values");
+        return desc;
+      }
 
     private:
-      void eval() override;
+      inline void eval() override {
+        const double tau_val = tau(q2_);
+
+        double num_e = 1., den_e = 1.;
+        for (size_t i = 0; i < a_e_.size(); ++i)
+          num_e += a_e_.at(i) * std::pow(tau_val, 1. + i);
+        for (size_t i = 0; i < b_e_.size(); ++i)
+          den_e += b_e_.at(i) * std::pow(tau_val, 1. + i);
+        const auto ge = num_e / den_e;
+
+        double num_m = 1., den_m = 1.;
+        for (size_t i = 0; i < a_m_.size(); ++i)
+          num_m += a_m_.at(i) * std::pow(tau_val, 1. + i);
+        for (size_t i = 0; i < b_m_.size(); ++i)
+          den_m += b_m_.at(i) * std::pow(tau_val, 1. + i);
+        const auto gm = MU * num_m / den_m;
+
+        setGEGM(ge, gm);
+      }
 
       const int mode_;
       std::vector<double> a_e_, b_e_;
       std::vector<double> a_m_, b_m_;
     };
-
-    ArringtonEtAl::ArringtonEtAl(const ParametersList& params) : Parameterisation(params), mode_(steer<int>("mode")) {
-      switch (mode_) {
-        case 0:  // original
-          a_e_ = {3.439, -1.602, 0.068};
-          b_e_ = {15.055, 48.061, 99.304, 0.012, 8.650};
-          a_m_ = {-1.465, 1.260, 0.262};
-          b_m_ = {9.627, 0., 0., 11.179, 13.245};
-          break;
-        case 1:  // fit of quoted Ge+dGe values
-          a_e_ = {4.309, -1.108, -0.324};
-          b_e_ = {15.340, 58.321, 124.11, 3.927, 0.589};
-          a_m_ = {-1.472, 1.210, 0.334};
-          b_m_ = {9.486, 0., 0., 9.440, 15.416};
-          break;
-        case 2:  // fit of quoted Ge-dGe values
-          a_e_ = {4.286, -1.281, -0.486};
-          b_e_ = {16.308, 54.535, 138.03, 7.005, 0.014};
-          a_m_ = {-1.374, 1.080, 0.124};
-          b_m_ = {10.003, 0., 0., 7.680, 9.009};
-          break;
-        case 3:  // fit of quoted Ge values
-          a_e_ = {4.109, -1.052, -0.375};
-          b_e_ = {15.602, 55.519, 123.96, 11.403, 1.931};
-          a_m_ = {-1.436, 1.196, 0.210};
-          b_m_ = {9.721, 0., 0., 9.623, 11.817};
-          break;
-      }
-    }
-
-    void ArringtonEtAl::eval() {
-      const double tau_val = tau(q2_);
-
-      double num_e = 1., den_e = 1.;
-      for (size_t i = 0; i < a_e_.size(); ++i)
-        num_e += a_e_.at(i) * pow(tau_val, 1. + i);
-      for (size_t i = 0; i < b_e_.size(); ++i)
-        den_e += b_e_.at(i) * pow(tau_val, 1. + i);
-      const auto ge = num_e / den_e;
-
-      double num_m = 1., den_m = 1.;
-      for (size_t i = 0; i < a_m_.size(); ++i)
-        num_m += a_m_.at(i) * pow(tau_val, 1. + i);
-      for (size_t i = 0; i < b_m_.size(); ++i)
-        den_m += b_m_.at(i) * pow(tau_val, 1. + i);
-      const auto gm = MU * num_m / den_m;
-
-      setGEGM(ge, gm);
-    }
-
-    ParametersDescription ArringtonEtAl::description() {
-      auto desc = Parameterisation::description();
-      desc.setDescription("Arrington et al.");
-      desc.add<int>("mode", 0).setDescription(
-          "Parameterisation mode "
-          "(0 = original, 1 = fit of quoted Ge+dGe values, "
-          "2 = fit of quoted Ge-dGe values, 3 = fit of quoted Ge values");
-      return desc;
-    }
   }  // namespace formfac
 }  // namespace cepgen
 using cepgen::formfac::ArringtonEtAl;
--- CepGen/FormFactors/MergellEtAl.cpp
+++ CepGen/FormFactors/MergellEtAl.cpp
@@ -1,6 +1,6 @@
 /*
  *  CepGen: a central exclusive processes event generator
- *  Copyright (C) 2013-2021  Laurent Forthomme
+ *  Copyright (C) 2019-2024  Laurent Forthomme
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -26,73 +26,67 @@ namespace cepgen {
     /// \cite Mergell:1995bf
     class MergellEtAl final : public Parameterisation {
     public:
-      explicit MergellEtAl(const ParametersList&);
+      explicit MergellEtAl(const ParametersList& params)
+          : Parameterisation(params),
+            a1rho_(steer<double>("a1rho")),
+            a2rho_(steer<double>("a2rho")),
+            b1rho_(steer<double>("b1rho")),
+            b2rho_(steer<double>("b2rho")),
+            c1rho_(steer<double>("c1rho")),
+            c2rho_(steer<double>("c2rho")),
+            d1rho_(steer<double>("d1rho")),
+            d2rho_(steer<double>("d2rho")),
+            inv_q20_(steer<double>("q20inv")),
+            lambda_sq_(steer<double>("LambdaSq")),
+            gamma_(steer<double>("gamma")) {}
 
-      static ParametersDescription description();
+      inline static ParametersDescription description() {
+        auto desc = Parameterisation::description();
+        desc.setDescription("Mergell et al.");
+        desc.add<double>("a1rho", 1.0317);
+        desc.add<double>("a2rho", 5.7824);
+        desc.add<double>("b1rho", 0.0875);
+        desc.add<double>("b2rho", 0.3907);
+        desc.add<double>("c1rho", 0.3176);
+        desc.add<double>("c2rho", 0.1422);
+        desc.add<double>("d1rho", 0.5496);
+        desc.add<double>("d2rho", 0.5362);
+        desc.add<double>("q20inv", 1. / 0.35);
+        desc.add<double>("LambdaSq", 9.733);
+        desc.add<double>("gamma", 2.148);
+        return desc;
+      }
 
     private:
-      void eval() override;
+      inline void eval() override {
+        const double log1 = std::pow(log((lambda_sq_ + q2_) * inv_q20_), -gamma_);  // L(t=-q2) function in ref.
+
+        // best fit parameterisation
+        const double d1_1 = 0.611 + q2_, d2_1 = 1.039 + q2_, d3_1 = 2.560 + q2_;
+        const double Fs1 = (9.464 / d1_1 - 9.054 / d2_1 - 0.410 / d3_1) * log1;
+        const double Fs2 = (-1.549 / d1_1 + 1.985 / d2_1 - 0.436 / d3_1) * log1;
+
+        const double log2 = std::pow(log((lambda_sq_ - 0.500) * inv_q20_), +gamma_);
+        const double log3 = std::pow(log((lambda_sq_ - 0.400) * inv_q20_), +gamma_);
+
+        const double d1_2 = 2.103 + q2_, d2_2 = 2.734 + q2_, d3_2 = 2.835 + q2_;
+        const double Fv1 =
+            (0.5 * (a1rho_ * log2 + b1rho_ * log3 * std::pow(1. + q2_ / c1rho_, -2)) / (1. + q2_ / d1rho_) -
+             38.885 / d1_2 + 425.007 / d2_2 - 389.742 / d3_2) *
+            log1;
+        const double Fv2 = (0.5 * (a2rho_ * log2 + b2rho_ * log3 / (1. + q2_ / c2rho_)) / (1. + q2_ / d2rho_) -
+                            73.535 / d1_2 + 83.211 / d2_2 - 29.467 / d3_2) *
+                           log1;
+
+        const double F1 = Fv1 + Fs1, F2 = Fv2 + Fs2;
+        setGEGM(F1 - tau(q2_) * F2, F1 + F2);
+      }
 
       const double a1rho_, a2rho_, b1rho_, b2rho_, c1rho_, c2rho_, d1rho_, d2rho_;
       const double inv_q20_;
       const double lambda_sq_;
       const double gamma_;
     };
-
-    MergellEtAl::MergellEtAl(const ParametersList& params)
-        : Parameterisation(params),
-          a1rho_(steer<double>("a1rho")),
-          a2rho_(steer<double>("a2rho")),
-          b1rho_(steer<double>("b1rho")),
-          b2rho_(steer<double>("b2rho")),
-          c1rho_(steer<double>("c1rho")),
-          c2rho_(steer<double>("c2rho")),
-          d1rho_(steer<double>("d1rho")),
-          d2rho_(steer<double>("d2rho")),
-          inv_q20_(steer<double>("q20inv")),
-          lambda_sq_(steer<double>("lambdaSq")),
-          gamma_(steer<double>("gamma")) {}
-
-    void MergellEtAl::eval() {
-      const double log1 = std::pow(log((lambda_sq_ + q2_) * inv_q20_), -gamma_);  // L(t=-q2) function in ref.
-
-      // best fit parameterisation
-      const double d1_1 = 0.611 + q2_, d2_1 = 1.039 + q2_, d3_1 = 2.560 + q2_;
-      const double Fs1 = (9.464 / d1_1 - 9.054 / d2_1 - 0.410 / d3_1) * log1;
-      const double Fs2 = (-1.549 / d1_1 + 1.985 / d2_1 - 0.436 / d3_1) * log1;
-
-      const double log2 = std::pow(log((lambda_sq_ - 0.500) * inv_q20_), +gamma_);
-      const double log3 = std::pow(log((lambda_sq_ - 0.400) * inv_q20_), +gamma_);
-
-      const double d1_2 = 2.103 + q2_, d2_2 = 2.734 + q2_, d3_2 = 2.835 + q2_;
-      const double Fv1 =
-          (0.5 * (a1rho_ * log2 + b1rho_ * log3 * std::pow(1. + q2_ / c1rho_, -2)) / (1. + q2_ / d1rho_) -
-           38.885 / d1_2 + 425.007 / d2_2 - 389.742 / d3_2) *
-          log1;
-      const double Fv2 = (0.5 * (a2rho_ * log2 + b2rho_ * log3 / (1. + q2_ / c2rho_)) / (1. + q2_ / d2rho_) -
-                          73.535 / d1_2 + 83.211 / d2_2 - 29.467 / d3_2) *
-                         log1;
-
-      const double F1 = Fv1 + Fs1, F2 = Fv2 + Fs2;
-      setGEGM(F1 - tau(q2_) * F2, F1 + F2);
-    }
-
-    ParametersDescription MergellEtAl::description() {
-      auto desc = Parameterisation::description();
-      desc.setDescription("Mergell et al.");
-      desc.add<double>("a1rho", 1.0317);
-      desc.add<double>("a2rho", 5.7824);
-      desc.add<double>("b1rho", 0.0875);
-      desc.add<double>("b2rho", 0.3907);
-      desc.add<double>("c1rho", 0.3176);
-      desc.add<double>("c2rho", 0.1422);
-      desc.add<double>("d1rho", 0.5496);
-      desc.add<double>("d2rho", 0.5362);
-      desc.add<double>("q20inv", 1. / 0.35);
-      desc.add<double>("lambdaSq", 9.733);
-      desc.add<double>("gamma", 2.148);
-      return desc;
-    }
   }  // namespace formfac
 }  // namespace cepgen
 using cepgen::formfac::MergellEtAl;
