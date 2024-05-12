6b828aeeb7b9e80c1711bb519efbaf5d38e04a9e
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-05-12 22:48:39 +0200

    Core: CMP0153-compliant replacement of exec_program to execute_process

--- CepGenAddOns/PythonWrapper/cmake/GenerateModulesDocs.cmake
+++ CepGenAddOns/PythonWrapper/cmake/GenerateModulesDocs.cmake
@@ -3,10 +3,10 @@ macro(cepgen_generate_python_descriptions)
     set(one_val NAME)
     set(multi_vals CATEGORIES)
     cmake_parse_arguments(ARG "${options}" "${one_val}" "${multi_vals}" ${ARGN})
-    exec_program(${MODULE_DOC_EXE}
-        ARGS --documentation-generator "\"text<modulesOnly,camelCaseModulesNames\""
-             --categories "\"${ARG_CATEGORIES}\""
-             --quiet
+    execute_process(COMMAND ${MODULE_DOC_EXE}
+        --documentation-generator "text<modulesOnly,camelCaseModulesNames"
+        --categories "${ARG_CATEGORIES}"
+        --quiet
         OUTPUT_VARIABLE modules)
     message(STATUS "Python... generating modules descriptions for ${ARG_NAME}")
     set(MODULE_DIR ${MODULE_DOC_OUTPUT}/${ARG_NAME})
@@ -18,15 +18,16 @@ macro(cepgen_generate_python_descriptions)
       "# Automatically generated collection of ${ARG_NAME} Python modules definitions"
       " for the steering of '${ARG_CATEGORIES}' category(ies).")
     foreach(_mod ${modules})
-        exec_program(${MODULE_DOC_EXE}
-            ARGS --documentation-generator python
-                 --categories "\"${ARG_CATEGORIES}\""
-                 --modules ${_mod}
-                 --output "${MODULE_DIR}/${_mod}_cfi.py"
-                 --quiet
+        string(STRIP ${_mod} mod_name)
+        execute_process(COMMAND ${MODULE_DOC_EXE}
+            --documentation-generator python
+            --categories "${ARG_CATEGORIES}"
+            --modules ${mod_name}
+            --output "${MODULE_DIR}/${mod_name}_cfi.py"
+            --quiet
             OUTPUT_VARIABLE dummy)
         if(ARG_LIST_ALL)
-            file(APPEND ${MODULE_DIR}/__init__.py "\nfrom .${_mod}_cfi import *")
+            file(APPEND ${MODULE_DIR}/__init__.py "\nfrom .${mod_name}_cfi import *")
         endif()
         message(DEBUG "      ... generated Python configuration file for ${ARG_NAME}/${_mod}")
     endforeach()
