30786f00489235a4a4a3abc1c67e4418f769d02e
Laurent Forthomme <laurent.forthomme@cern.ch>
2024-07-19 11:48:42 +0200

    Tests: added other test modes for event content exporter

--- test/physics/event_writer.cc
+++ test/physics/event_writer.cc
@@ -1,6 +1,6 @@
 /*
  *  CepGen: a central exclusive processes event generator
- *  Copyright (C) 2013-2023  Laurent Forthomme
+ *  Copyright (C) 2018-2024  Laurent Forthomme
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,18 +23,17 @@
 #include "CepGen/Physics/PDG.h"
 #include "CepGen/Utils/ArgumentsParser.h"
 #include "CepGen/Utils/Test.h"
+#include "CepGenAddOns/Common/EventUtils.h"
 
 using namespace std;
-using namespace cepgen;
 
 int main(int argc, char* argv[]) {
-  initialise();
+  cepgen::initialise();
 
   string type;
   bool list;
-
-  ArgumentsParser(argc, argv)
-      .addOptionalArgument("format", "type of format to build", &type, "hepmc")
+  cepgen::ArgumentsParser(argc, argv)
+      .addOptionalArgument("format,f", "type of format to build", &type, "hepmc")
       .addOptionalArgument("list,l", "list all formats", &list, false)
       .parse();
 
@@ -42,30 +41,47 @@ int main(int argc, char* argv[]) {
     CG_LOG.log([](auto& log) {
       log << "List of export modules available:\n"
           << "=================================";
-      for (const auto& mod : EventExporterFactory::get().modules())
+      for (const auto& mod : cepgen::EventExporterFactory::get().modules())
         log << "\n" << mod;
     });
     return 0;
   }
 
-  auto writer = EventExporterFactory::get().build(type);
-  writer->setCrossSection(Value{1., 2.});
+  auto writer = cepgen::EventExporterFactory::get().build(type);
+  writer->setCrossSection(cepgen::Value{1., 2.});
 
-  Event ev;
+  {  // first test: simple event content
+    cepgen::Event ev;
 
-  Particle p1(Particle::IncomingBeam1, PDG::proton);
-  p1.setMomentum(1., -15., 100.);
-  p1.setStatus(Particle::Status::Incoming);
-  ev.addParticle(p1);
+    cepgen::Particle p1(cepgen::Particle::IncomingBeam1, cepgen::PDG::proton);
+    p1.setMomentum(1., -15., 100.);
+    p1.setStatus(cepgen::Particle::Status::Incoming);
+    ev.addParticle(p1);
 
-  Particle p2(Particle::IncomingBeam2, PDG::electron);
-  p2.setMomentum(10., 5., 3200.);
-  p2.setStatus(Particle::Status::Incoming);
-  ev.addParticle(p2);
+    cepgen::Particle p2(cepgen::Particle::IncomingBeam2, cepgen::PDG::electron);
+    p2.setMomentum(10., 5., 3200.);
+    p2.setStatus(cepgen::Particle::Status::Incoming);
+    ev.addParticle(p2);
 
-  ev.dump();
-
-  *writer << ev;
+    const auto ev_old = ev;
+    CG_DEBUG("main") << "Event content:\n" << ev;
+    *writer << ev;
+    CG_TEST(ev == ev_old, "[" + type + "] event content preservation by output (simple event)");
+  }
+  {  // second test: simple event content with parentage
+    auto ev = cepgen::Event::minimal();
+    const auto ev_old = ev;
+    CG_DEBUG("main") << "Event content:\n" << ev;
+    *writer << ev;
+    CG_TEST(ev == ev_old, "[" + type + "] event content preservation by output (simple+parentage event)");
+  }
+  {  // third test: realistic lpair event content
+    auto ev = cepgen::utils::generateLPAIREvent();
+    const auto ev_old = ev;
+    CG_DEBUG("main") << "Event content:\n" << ev;
+    *writer << ev;
+    CG_TEST(ev == ev_old, "[" + type + "] event content preservation by output (lpair event)");
+  }
 
   CG_TEST_SUMMARY;
 }
