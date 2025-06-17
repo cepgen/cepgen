91b316512cce16f849c53d532199ea7bc3bb4b35
Laurent Forthomme <laurent.forthomme@cern.ch>
2025-06-17 10:59:33 +0200

    Tauola: filter to ensure the decay of taus when running the algorithm

--- addons/TauolaWrapper/src/TauolaFilter.cpp
+++ addons/TauolaWrapper/src/TauolaFilter.cpp
@@ -52,12 +52,17 @@ namespace cepgen::tauola {
         Tauola::setUnits(Tauola::GEV, Tauola::MM);
         auto sc = utils::StreamCollector(buf);
         Tauola::initialize();
-        Tauola::setSeed(seed_, 2. * seed_, 4. * seed_);
-        Tauola::momentum_conservation_threshold = steer<double>("momentumConservationThreshold");
       }
       CG_INFO("TauolaFilter") << "Tauola initialised. Output:\n" << std::string(80, '-') << buf << std::string(80, '-');
       if (!Tauola::getIsTauolaIni())
         throw CG_FATAL("TauolaFilter") << "Tauola was not properly initialised!";
+      // default parameters
+      Tauola::setSeed(seed_, 2. * seed_, 4. * seed_);
+      Tauola::momentum_conservation_threshold = steer<double>("momentumConservationThreshold");
+      Tauola::setDecayingParticle(steer<int>("decayingParticle"));
+      Tauola::setSameParticleDecayMode(steer<int>("sameParticleDecayMode"));
+      Tauola::setOppositeParticleDecayMode(steer<int>("oppositeParticleDecayMode"));
+      // list of polarisation and spin correlations-specific parameters
       if (const auto& pol_states = steer<ParametersList>("polarisations"); !pol_states.empty()) {  // spin correlations
         if (pol_states.has<bool>("full"))
           Tauola::spin_correlation.setAll(pol_states.get<bool>("full"));
@@ -71,17 +76,14 @@ namespace cepgen::tauola {
         pol_states.fill("W_PLUS", Tauola::spin_correlation.W_PLUS);
         pol_states.fill("W_MINUS", Tauola::spin_correlation.W_MINUS);
       }
-      if (const auto& rad_states = steer<ParametersList>("radiations"); !rad_states.empty()) {  // radiation states
+      // list of enabled radiation states
+      if (const auto& rad_states = steer<ParametersList>("radiations"); !rad_states.empty()) {
         if (rad_states.has<bool>("enable"))
           Tauola::setRadiation(rad_states.get<bool>("enable"));
         if (const auto rad_cutoff = rad_states.get<double>("cutoff", 0.01); rad_cutoff > 0.)
           Tauola::setRadiationCutOff(rad_cutoff);
       }
-      //--- default parameters
-      Tauola::setDecayingParticle(steer<int>("decayingParticle"));
-      Tauola::setSameParticleDecayMode(steer<int>("sameParticleDecayMode"));
-      Tauola::setOppositeParticleDecayMode(steer<int>("oppositeParticleDecayMode"));
-      //--- list of tau decay branching fractions
+      // list of tau decay branching fractions
       for (const auto& br_per_mode : steer<std::vector<ParametersList> >("branchingRatios")) {
         const auto mode = br_per_mode.get<int>("mode");
         if (const auto br = br_per_mode.get<double>("branchingRatio"); br > 0.) {
@@ -97,9 +99,25 @@ namespace cepgen::tauola {
       desc.setDescription("Tauola interface");
       desc.add("debug", false).setDescription("debugging mode");
       desc.add("decayingParticle", 15).setDescription("pdg id of the particle to decay (+-15 typically)");
-      desc.add("sameParticleDecayMode", -1)
+      desc.add("sameParticleDecayMode", static_cast<int>(Tauola::All))
+          .allow(Tauola::All, "all")
+          .allow(Tauola::ElectronMode, "electron")
+          .allow(Tauola::MuonMode, "muon")
+          .allow(Tauola::PionMode, "pion")
+          .allow(Tauola::RhoMode, "rho")
+          .allow(Tauola::A1Mode, "A_1")
+          .allow(Tauola::KMode, "K")
+          .allow(Tauola::KStarMode, "K*")
           .setDescription("uniformise the decay mode of all particle with the one given in 'decayingParticle'");
-      desc.add("oppositeParticleDecayMode", -1)
+      desc.add("oppositeParticleDecayMode", static_cast<int>(Tauola::All))
+          .allow(Tauola::All, "all")
+          .allow(Tauola::ElectronMode, "electron")
+          .allow(Tauola::MuonMode, "muon")
+          .allow(Tauola::PionMode, "pion")
+          .allow(Tauola::RhoMode, "rho")
+          .allow(Tauola::A1Mode, "A_1")
+          .allow(Tauola::KMode, "K")
+          .allow(Tauola::KStarMode, "K*")
           .setDescription(
               "uniformise the decay mode of all particle with opposite charge to the one given in 'decayingParticle'");
       desc.add("momentumConservationThreshold", 1.e-6)
@@ -135,9 +153,12 @@ namespace cepgen::tauola {
     bool run(Event& event, double& weight, bool /*fast*/) override {
       weight = 1.;
       CepGenEvent hepmc_event(event);  // conversion to a HepMC3 format
+      const auto hepmc_event_size_before = hepmc_event.particles_size();
       TauolaHepMC3Event tauola_event(&hepmc_event);
       tauola_event.decayTaus();
       hepmc_event.merge(event);  // merge everything back into the original event
+      if (hepmc_event.particles_size() == hepmc_event_size_before)
+        return false;
       return true;
     }
   };
